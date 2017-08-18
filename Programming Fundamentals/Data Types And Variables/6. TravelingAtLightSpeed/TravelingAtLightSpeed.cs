8063f84fbeb3fc3e0ef4106e2ba11cb9adf5b5de
Viktor Levin <viktortus@yahoo.com>
2017-08-18 14:57:52 +0200

    Programming Fundamentals added

---
+++ Programming Fundamentals/Data Types And Variables/6. TravelingAtLightSpeed/TravelingAtLightSpeed.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TravelingAtLightSpeed
+{
+    class TravelingAtLightSpeed
+    {
+        static void Main()
+        {
+            decimal lightYears = decimal.Parse(Console.ReadLine());
+
+            decimal kilometers = lightYears * 9450000000000;
+            decimal speedOfLight = 300000;
+
+            decimal seconds = kilometers / speedOfLight;
+
+            decimal minutes = seconds / 60;
+            seconds %= 60;
+            decimal hours = minutes / 60;
+            minutes %= 60;
+            decimal days = hours / 24;
+            hours %= 24;
+            decimal weeks = days / 7;
+            days %= 7;
+
+            Console.WriteLine($"{(int)weeks} weeks");
+            Console.WriteLine($"{(int)days} days");
+            Console.WriteLine($"{(int)hours} hours");
+            Console.WriteLine($"{(int)minutes} minutes");
+            Console.WriteLine($"{(int)seconds} seconds");
+        }
+    }
+}
---
+++ Programming Fundamentals/Text And Other Types/1. ASCIIString/ASCIIString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ASCIIString
+{
+    class ASCIIString
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string text = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                char character = (char) num;
+                text += character;
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
