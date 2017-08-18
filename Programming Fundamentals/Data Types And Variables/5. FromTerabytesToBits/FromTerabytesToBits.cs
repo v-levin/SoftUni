75e43a25a13cd19cb44f338564d77eee3876d462
Viktor Levin <viktortus@yahoo.com>
2017-08-18 14:46:16 +0200

    Programming Fundamentals added

---
+++ Programming Fundamentals/Data Types And Variables/5. FromTerabytesToBits/FromTerabytesToBits.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FromTerabytesToBits
+{
+    class FromTerabytesToBits
+    {
+        static void Main()
+        {
+            double teraBytes = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(teraBytes * 1024 * 1024 * 1024 * 1024 * 8);
+
+        }
+    }
+}
