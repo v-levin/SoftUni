f51d2d4ebd8e281d760fb8c309256592f995b680
Viktor Levin <viktortus@yahoo.com>
2017-08-22 15:19:21 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13. STRINGS AND TEXT PROCESSING/4.Palindromes/Palindromes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4.Palindromes
+{
+    public class Palindromes
+    {
+        public static void Main()
+        {
+            var result = new List<string>();
+
+            var text = Console.ReadLine();
+
+            var words = text.Split(new[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                var palindrom = Palindrom(word);
+
+                if (palindrom)
+                {
+                    result.Add(word);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result.Distinct().OrderBy(w => w)));
+        }
+
+        private static bool Palindrom(string word)
+        {
+
+            var reversedWord = new StringBuilder();
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord.Append(word[i]);
+            }
+
+            if (word == reversedWord.ToString())
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
