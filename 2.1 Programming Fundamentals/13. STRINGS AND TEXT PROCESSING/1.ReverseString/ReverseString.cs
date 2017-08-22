fa14385b053615513840620e8987fda38eb8392c
Viktor Levin <viktortus@yahoo.com>
2017-08-22 13:23:20 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13. STRINGS AND TEXT PROCESSING/1.ReverseString/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1.ReverseString
+{
+    public class ReverseStrings
+    {
+        public static void Main()
+        {
+            var inputString = Console.ReadLine();
+
+            Console.WriteLine(ReverseString(inputString));
+        }
+
+        private static string ReverseString(string inputString)
+        {
+            var charArray = inputString.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            return new string(charArray);
+        }
+    }
+}
