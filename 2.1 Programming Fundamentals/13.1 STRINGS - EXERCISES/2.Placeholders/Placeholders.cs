c5ee207119cf1e4f5814c223bc2950cce03a11f0
Viktor Levin <viktortus@yahoo.com>
2017-08-22 20:34:02 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.1 STRINGS - EXERCISES/2.Placeholders/Placeholders.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2.Placeholders
+{
+    public class Placeholders
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+
+            var result = string.Empty;
+
+            while (inputLine != "end")
+            {
+                var inputParams = inputLine.Split(new[] { '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+                var sentence = inputParams[0];
+                var elements = inputParams[1].Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    var currentPlaceholder = "{" + i + "}";
+                    sentence = sentence.Replace(currentPlaceholder, elements[i]);
+                }
+
+                Console.WriteLine(sentence);
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
