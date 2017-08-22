7d13b7276298eb7508e53a60480e409c1bab9adc
Viktor Levin <viktortus@yahoo.com>
2017-08-22 17:16:09 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.1 STRINGS - EXERCISES/1.FindTheLetter/FindTheLetter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1.FindTheLetter
+{
+    public class FindTheLetter
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var elements = Console.ReadLine().Split();
+
+            var letter = elements[0];
+            var integerN = int.Parse(elements[1]);
+            
+            var index = -1;
+
+            for (int i = 0; i < integerN; i++)
+            {
+                index = text.IndexOf(letter, index + 1);
+            }
+
+            if (index != -1)
+            {
+                Console.WriteLine(index);
+            }
+            else
+            {
+                Console.WriteLine("Find the letter yourself!");
+            }
+        }
+    }
+}
