8aef66e72d17bc07b73e415db135ca16faaf2b03
Viktor Levin <viktortus@yahoo.com>
2017-08-23 17:59:15 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.1 STRINGS - EXERCISES/7.CapitalizeWords/CapitalizeWords.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.CapitalizeWords
+{
+    public class CapitalizeWords
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var result = new List<string>();
+
+            while (inputLine != "end")
+            {
+                var inputParams = inputLine.Split(new[] { ' ', '.', ',', '!', '?', ':', '@', '$', '%', '*', '"', ';', '(', ')', '[', ']', '{', '}', '|', '#', '^', '&', '-', '+' }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in inputParams)
+                {
+                    result.Add(UppercaseFirst(word));
+                }
+
+                Console.WriteLine(string.Join(", ", result));
+
+                result.Clear();
+
+                inputLine = Console.ReadLine();
+            }
+        }
+
+        private static string UppercaseFirst(string word)
+        {
+            word = word.ToLower();
+            char[] a = word.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
