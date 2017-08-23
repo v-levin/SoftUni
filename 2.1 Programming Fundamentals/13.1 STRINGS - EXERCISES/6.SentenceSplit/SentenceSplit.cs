3b23f37f744d2db203dec128116a3332349e4139
Viktor Levin <viktortus@yahoo.com>
2017-08-23 16:49:31 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.1 STRINGS - EXERCISES/6.SentenceSplit/SentenceSplit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6.SentenceSplit
+{
+    public class SentenceSplit
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var delimiters = Console.ReadLine();
+
+            var splitedSentence = inputLine.Split(new string[] { delimiters }, StringSplitOptions.None);
+            
+            Console.WriteLine($"[{string.Join(", ", splitedSentence)}]");
+        }
+    }
+}
