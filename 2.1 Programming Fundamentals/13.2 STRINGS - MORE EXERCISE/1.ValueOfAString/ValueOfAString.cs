6213261466c8885629cefd0a19293bdd13dd94ba
Viktor Levin <viktortus@yahoo.com>
2017-08-23 22:38:38 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.2 STRINGS - MORE EXERCISE/1.ValueOfAString/ValueOfAString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace _1.ValueOfAString
+{
+    public class ValueOfAString
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var charsToSum = Console.ReadLine();
+
+            var sumOfLowercase = 0;
+            var sumOfUppercase = 0;
+
+            foreach (var letter in inputLine)
+            {
+                if (char.IsLower(letter))
+                {
+                    sumOfLowercase += letter;
+                }
+                else if (char.IsUpper(letter))
+                {
+                    sumOfUppercase += letter;
+                }
+            }
+
+            if (charsToSum == "LOWERCASE")
+            {
+                PrintSum(sumOfLowercase);
+            }
+            else if (charsToSum == "UPPERCASE")
+            {
+                PrintSum(sumOfUppercase);
+            }
+        }
+
+        private static void PrintSum(int sum)
+        {
+            Console.WriteLine($"The total sum is: {sum}");
+        }
+    }
+}
