044e3ae55659c3466ebbcdf73e54652458be6372
Viktor Levin <viktortus@yahoo.com>
2017-08-24 18:10:57 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.2 STRINGS - MORE EXERCISE/3.SerializeString/SerializeString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.SerializeString
+{
+    public class SerializeString
+    {
+        public static void Main()
+        {
+            var letters = new Dictionary<string, List<int>>();
+
+            var inputLine = Console.ReadLine();
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                var letter = inputLine[i].ToString();
+
+                if (!letters.ContainsKey(letter))
+                {
+                    letters[letter] = new List<int>();
+                }
+
+                letters[letter].Add(i);
+            }
+
+            foreach (var letter in letters)
+            {
+                Console.WriteLine($"{letter.Key}:{string.Join("/", letter.Value)}");               
+            }
+        }
+    }
+}
