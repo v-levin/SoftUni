e8e8a50564b2a4b888e42d0dfeb77ba09e9b76b1
Viktor Levin <viktortus@yahoo.com>
2017-08-24 18:54:24 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.2 STRINGS - MORE EXERCISE/4.DeserializeString/DeserializeString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.DeserializeString
+{
+    public class DeserializeString
+    {
+        public static void Main()
+        {
+            var indexOfLetters = new Dictionary<int, string>();
+
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                var inputParams = inputLine.Split(':');
+                var letter = inputParams[0];
+                var positions = inputParams[1].Split('/');
+
+                foreach (var position in positions)
+                {
+                    var index = int.Parse(position);
+
+                    indexOfLetters[index] = letter;
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var letter in indexOfLetters.OrderBy(l => l.Key))
+            {
+                Console.Write(letter.Value);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
