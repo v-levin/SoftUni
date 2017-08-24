986a2c41d9d36cd71d18c87ec58079b8dda4b097
Viktor Levin <viktortus@yahoo.com>
2017-08-24 15:00:42 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.2 STRINGS - MORE EXERCISE/2.DiamondProblem/DiamondProblem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.DiamondProblem
+{
+    public class DiamondProblem
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+
+            var diamonds = new List<string>();
+
+            var tempString = string.Empty;
+
+            while (inputLine.Contains("<"))
+            {
+                var openedDiamondIndex = inputLine.IndexOf("<");
+                tempString = inputLine.Substring(openedDiamondIndex, inputLine.Length - openedDiamondIndex);
+
+                if (tempString.Contains(">"))
+                {
+                    var closedDiamondIndex = tempString.IndexOf(">");
+
+                    tempString = tempString.Substring(0, closedDiamondIndex + 1);
+
+                    diamonds.Add(tempString);
+                }
+
+                inputLine = inputLine.Replace(tempString, string.Empty);
+            }
+            
+
+            if (diamonds.Count > 0)
+            {
+                foreach (var diamond in diamonds)
+                {
+                    var sum = 0;
+
+                    foreach (var character in diamond)
+                    {
+                        var digit = 0;
+                        if (int.TryParse(character.ToString(), out digit))
+                        {
+                            sum += digit;
+                        }
+                    }
+
+                    Console.WriteLine($"Found {sum} carat diamond");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time");
+            }
+        }
+    }
+}
