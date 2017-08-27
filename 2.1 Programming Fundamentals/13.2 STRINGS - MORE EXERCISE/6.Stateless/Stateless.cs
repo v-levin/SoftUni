8e565b5d61401c49cbf7ce6ee51c0fc8eeadc633
Viktor Levin <viktortus@yahoo.com>
2017-08-27 13:54:04 +0200

    task added

---
+++ 2.1 Programming Fundamentals/13.2 STRINGS - MORE EXERCISE/6.Stateless/Stateless.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _6.Stateless
+{
+    public class Stateless
+    {
+        public static void Main()
+        {
+            var state = Console.ReadLine();
+
+            while (state != "collapse")
+            {
+                var fiction = Console.ReadLine();
+
+                while (fiction.Length > 0)
+                {
+                    var index = 0;
+                    while (state.Contains(fiction))
+                    {
+                        index = state.IndexOf(fiction);
+                        state = state.Remove(index, fiction.Length).Trim();
+                    }
+
+                    fiction = RemoveFirstAndLastElement(fiction);
+                }
+
+                if (state.Length > 0)
+                {
+                    Console.WriteLine(state);
+                }
+                else
+                {
+                    Console.WriteLine("(void)");
+                }
+
+                state = Console.ReadLine();
+            }
+        }
+
+        private static string RemoveFirstAndLastElement(string fiction)
+        {
+            fiction = fiction.Remove(0, 1);
+
+            if (fiction.Length > 0)
+            {
+                fiction = fiction.Remove(fiction.Length - 1, 1);
+            }
+
+            return fiction;
+        }
+    }
+}
