78cc50b975e860f8e60b02e4f9b46c8454007315
Viktor Levin <viktortus@yahoo.com>
2017-08-29 23:00:43 +0200

    task added

---
+++ 2.1 Programming Fundamentals/14. REGULAR EXPRESSIONS (REGEX)/2.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2.MatchPhoneNumber
+{
+    public class MatchPhoneNumber
+    {
+        public static void Main()
+        {
+            var regex = @"\+359(\s|-)2\1\d{3}\1\d{4}\b";
+
+            var phones = Console.ReadLine();
+
+            var phoneMatches = Regex.Matches(phones, regex);
+
+            var matchedPhones = phoneMatches.Cast<Match>().Select(p => p.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
