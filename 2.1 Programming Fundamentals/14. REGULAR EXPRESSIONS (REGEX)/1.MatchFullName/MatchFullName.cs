135dda1b39ba156a6fc83f45cf0539ca33310271
Viktor Levin <viktortus@yahoo.com>
2017-08-29 22:33:09 +0200

    task added

---
+++ 2.1 Programming Fundamentals/14. REGULAR EXPRESSIONS (REGEX)/1.MatchFullName/MatchFullName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.MatchFullName
+{
+    public class MatchFullName
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine();
+
+            var pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            var regex = new Regex(pattern);
+
+            var matchedNames = regex.Matches(names);
+
+            foreach (Match name in matchedNames)
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
