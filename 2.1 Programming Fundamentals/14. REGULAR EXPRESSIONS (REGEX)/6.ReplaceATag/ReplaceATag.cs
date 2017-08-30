e30e8e621accf8f0dd7ab40d37e8a4177ebe26bd
Viktor Levin <viktortus@yahoo.com>
2017-08-30 14:20:56 +0200

    task added

---
+++ 2.1 Programming Fundamentals/14. REGULAR EXPRESSIONS (REGEX)/6.ReplaceATag/ReplaceATag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _6.ReplaceATag
+{
+    public class ReplaceATag
+    {
+        public static void Main()
+        {
+
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                var regex = @"<a\s+href=(.+?)>(.+?)<\/a>";
+
+                var replacement = @"[URL href=$1]$2[/URL]";
+
+                var replaced = Regex.Replace(inputLine, regex, replacement);
+
+                Console.WriteLine(replaced);
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
