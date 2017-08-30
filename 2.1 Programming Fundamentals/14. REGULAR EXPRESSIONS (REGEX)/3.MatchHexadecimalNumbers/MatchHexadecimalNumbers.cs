eb52047667e124a5be9c537bd1c5b2c18b1e9d65
Viktor Levin <viktortus@yahoo.com>
2017-08-30 13:16:48 +0200

    task added

---
+++ 2.1 Programming Fundamentals/14. REGULAR EXPRESSIONS (REGEX)/3.MatchHexadecimalNumbers/MatchHexadecimalNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3.MatchHexadecimalNumbers
+{
+    public class MatchHexadecimalNumbers
+    {
+        public static void Main()
+        {
+            var regex = @"\b(?:0x)?[0-9A-F]+\b";
+
+            var numbersString = Console.ReadLine();
+
+            var numbers = Regex.Matches(numbersString, regex).Cast<Match>().Select(n => n.Value).ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
