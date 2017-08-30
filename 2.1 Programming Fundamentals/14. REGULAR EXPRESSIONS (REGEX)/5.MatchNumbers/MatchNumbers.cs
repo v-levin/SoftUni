b68e10c89d2b78369f94e3d92c6f0ba7bb042b54
Viktor Levin <viktortus@yahoo.com>
2017-08-30 14:01:50 +0200

    task added

---
+++ 2.1 Programming Fundamentals/14. REGULAR EXPRESSIONS (REGEX)/5.MatchNumbers/MatchNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _5.MatchNumbers
+{
+    public class MatchNumbers
+    {
+        public static void Main()
+        {
+            var regex = @"(^|(?<=\s))-?\d+(?:\.\d+)?($|(?=\s))";
+
+            var numberStrings = Console.ReadLine();
+
+            var numbers = Regex.Matches(numberStrings, regex).Cast<Match>().Select(n => n.Value).ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
