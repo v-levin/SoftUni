5c7deec1eb051ad51306e301f6c77b18d9efe2a0
Viktor Levin <viktortus@yahoo.com>
2017-08-30 13:46:36 +0200

    task added

---
+++ 2.1 Programming Fundamentals/14. REGULAR EXPRESSIONS (REGEX)/4.MatchDates/MatchDates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _4.MatchDates
+{
+    public class MatchDates
+    {
+        public static void Main()
+        {
+            var regex = @"\b(\d{2})([-.\/])([A-Z][a-z]{2})\2(\d{4})\b";
+
+            var datesString = Console.ReadLine();
+            
+            var dates = Regex.Matches(datesString, regex);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups[1].Value;
+                var month = date.Groups[3].Value;
+                var year = date.Groups[4].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
