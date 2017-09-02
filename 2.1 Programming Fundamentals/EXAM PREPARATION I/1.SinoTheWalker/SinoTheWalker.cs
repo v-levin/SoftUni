974f9ded4475f8d29058eb7d094ddcdec92bbe30
Viktor Levin <viktortus@yahoo.com>
2017-09-02 13:52:51 +0200

    task added

---
+++ 2.1 Programming Fundamentals/EXAM PREPARATION I/1.SinoTheWalker/SinoTheWalker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1.SinoTheWalker
+{
+    public class SinoTheWalker
+    {
+        public static void Main()
+        {
+            var timeFormat = "HH:mm:ss";
+            var timeLeaves = DateTime.ParseExact(Console.ReadLine(), timeFormat, CultureInfo.InvariantCulture);
+            var numberOfSteps = decimal.Parse(Console.ReadLine());
+            var secondsPerStep = decimal.Parse(Console.ReadLine());
+
+            var secondsInADay = 60 * 60 * 24;
+            var totalSeconds = (int)(numberOfSteps * secondsPerStep % secondsInADay);
+
+            var arrivalTime = timeLeaves.AddSeconds(totalSeconds);
+
+            Console.WriteLine($"Time Arrival: {arrivalTime.ToString(timeFormat)}");
+        }
+    }
+}
