76c44a87b893ea7f1c8fa56b6ad9b2586c243b94
Viktor Levin <viktortus@yahoo.com>
2017-09-04 19:28:05 +0200

    task added

---
+++ 2.1 Programming Fundamentals/EXAM PREPARATION II/2.Ladybugs/Ladybugs.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.Ladybugs
+{
+    public class Ladybugs
+    {
+        public static void Main()
+        {
+            var sizeOfField = int.Parse(Console.ReadLine());
+            var field = new long[sizeOfField];
+            var indexesOfLadybugs = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+            
+            foreach (var index in indexesOfLadybugs)
+            {
+                if (index < 0 || index >= sizeOfField)
+                {
+                    continue;
+                }
+
+                field[index] = 1;
+            }
+
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                var commandParams = command.Split();
+                var currentLadybugIndex = int.Parse(commandParams[0]);
+                var direction = commandParams[1];
+                var flyLength = int.Parse(commandParams[2]);
+
+                if (currentLadybugIndex < 0 || currentLadybugIndex >= sizeOfField)
+                {
+                    continue;
+                }
+
+                if (field[currentLadybugIndex] == 0)
+                {
+                    continue;
+                }
+
+                field[currentLadybugIndex] = 0;
+                var position = currentLadybugIndex;
+
+                while (true)
+                {
+                    if (direction == "right")
+                    {
+                        position += flyLength;
+                    }
+                    else if (direction == "left")
+                    {
+                        position -= flyLength;
+                    }
+
+                    if (position < 0 || position >= sizeOfField)
+                    {
+                        break;
+                    }
+
+                    if (field[position] == 0)
+                    {
+                        field[position] = 1;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
