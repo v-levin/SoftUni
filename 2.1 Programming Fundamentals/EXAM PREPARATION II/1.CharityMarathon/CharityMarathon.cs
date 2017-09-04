f095f167a6bf3801a8f7bf2b0dab3606f4683ca7
Viktor Levin <viktortus@yahoo.com>
2017-09-04 17:26:21 +0200

    task added

---
+++ 2.1 Programming Fundamentals/EXAM PREPARATION II/1.CharityMarathon/CharityMarathon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1.CharityMarathon
+{
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            var marathonLengthInDays = int.Parse(Console.ReadLine());
+            var numberOfRunners = int.Parse(Console.ReadLine());
+            var averageNumberOfLaps = int.Parse(Console.ReadLine());
+            var trackLength = int.Parse(Console.ReadLine());
+            var trackCapacity = int.Parse(Console.ReadLine());
+            var donatedMoneyPerKilometer = decimal.Parse(Console.ReadLine());
+
+            trackCapacity = trackCapacity * marathonLengthInDays;
+
+            numberOfRunners = Math.Min(trackCapacity, numberOfRunners);
+
+            var totalMeters = (decimal)numberOfRunners * averageNumberOfLaps * trackLength;
+            var totalKilometers = totalMeters / 1000;
+            var moneyRaised = totalKilometers * donatedMoneyPerKilometer;
+
+            Console.WriteLine($"Money raised: {moneyRaised:F2}");
+        }
+    }
+}
