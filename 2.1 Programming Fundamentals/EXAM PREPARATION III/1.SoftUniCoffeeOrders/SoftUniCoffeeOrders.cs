e885dae99caaf757d10d9d4445466b462ad7db44
Viktor Levin <viktortus@yahoo.com>
2017-09-06 14:32:42 +0200

    task added

---
+++ 2.1 Programming Fundamentals/EXAM PREPARATION III/1.SoftUniCoffeeOrders/SoftUniCoffeeOrders.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1.SoftUniCoffeeOrders
+{
+    public class SoftUniCoffeeOrders
+    {
+        public static void Main()
+        {
+            var countOfOrders = int.Parse(Console.ReadLine());
+
+            var totalPrice = 0M;
+            for (int i = 0; i < countOfOrders; i++)
+            {
+                var pricePerCapsule = decimal.Parse(Console.ReadLine());
+                var orderDate = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InstalledUICulture);
+                var capsulesCount = int.Parse(Console.ReadLine());
+
+                var month = orderDate.Month;
+                var year = orderDate.Year;
+
+                var daysInMonth = DateTime.DaysInMonth(year, month);
+
+                var coffeePrice = ((daysInMonth * (long)capsulesCount) * pricePerCapsule);
+
+                Console.WriteLine($"The price for the coffee is: ${coffeePrice:F2}");
+
+                totalPrice += coffeePrice;
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
