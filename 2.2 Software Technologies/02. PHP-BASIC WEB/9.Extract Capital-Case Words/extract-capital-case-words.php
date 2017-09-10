4463d77a453dbc848f9cea5affb5f97366155a43
Viktor Levin <viktortus@yahoo.com>
2017-09-10 23:19:24 +0200

    task added

---
+++ 2.2 Software Technologies/02. PHP-BASIC WEB/9.Extract Capital-Case Words/extract-capital-case-words.php
@@ -0,0 +1,20 @@
+<?php
+$capitalCaseWords = "";
+if (isset($_GET["text"])) {
+    $text = $_GET["text"];
+    preg_match_all('/\w+/', $text, $words);
+    $words = $words[0];
+
+    $upperWords = array_filter($words, function($word){
+        return strtoupper($word) == $word;
+    });
+
+    $capitalCaseWords = implode(', ', $upperWords);
+}
+?>
+
+<form>
+    <textarea rows="10" name="text"><?= $capitalCaseWords ?></textarea> <br>
+    <input type="submit" value="Extract">
+</form>
+
