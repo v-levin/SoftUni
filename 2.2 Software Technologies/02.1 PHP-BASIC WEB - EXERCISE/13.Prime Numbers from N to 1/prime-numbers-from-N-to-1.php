0e3ea8c0d45c86253a5853aa9d077a5d5fdae91e
Viktor Levin <viktortus@yahoo.com>
2017-09-11 20:27:37 +0200

    task added

---
+++ 2.2 Software Technologies/02.1 PHP-BASIC WEB - EXERCISE/13.Prime Numbers from N to 1/prime-numbers-from-N-to-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+
+</head>
+<body>
+<form>
+    N: <input type="text" name="num" />
+    <input type="submit" />
+</form>
+<?php
+
+if (isset($_GET["num"])) {
+    $num = intval($_GET["num"]);
+
+    for ($i = $num; $i > 1; $i--) {
+        $isPrime = true;
+
+        for ($j = 2; $j <= sqrt($i); $j++) {
+            if ($i % $j == 0) {
+                $isPrime = false;
+                break;
+            }
+        }
+
+        if ($isPrime) {
+            echo $i . " ";
+        }
+    }
+}
+
+?>
+</body>
+</html>
