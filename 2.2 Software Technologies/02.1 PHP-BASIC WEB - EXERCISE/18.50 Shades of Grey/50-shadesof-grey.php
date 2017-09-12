02b6e534a69077e0db849f6ab0bb827d3364c47f
Viktor Levin <viktortus@yahoo.com>
2017-09-12 14:42:06 +0200

    task added

---
+++ 2.2 Software Technologies/02.1 PHP-BASIC WEB - EXERCISE/18.50 Shades of Grey/50-shadesof-grey.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+    <style>
+        div {
+            display: inline-block;
+            margin: 5px;
+            width: 20px;
+            height: 20px;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+$color = 0;
+for ($row = 0; $row < 5; $row++) {
+    $color = 51 * $row;
+
+    for ($col = 0; $col < 10; $col++) {
+        echo "<div style='background-color: rgb($color, $color, $color)'></div>";
+        $color += 5;
+    }
+    echo "<br>";
+}
+
+?>
+</body>
+</html>
