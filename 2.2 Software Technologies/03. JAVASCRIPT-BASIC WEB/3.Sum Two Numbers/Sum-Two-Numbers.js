7d4bd4c607e7c8f4f371f5e20f0f7623cc3de8ca
Viktor Levin <viktortus@yahoo.com>
2017-09-15 17:53:00 +0200

    task added

---
+++ 2.2 Software Technologies/03. JAVASCRIPT-BASIC WEB/3.Sum Two Numbers/Sum-Two-Numbers.js
@@ -0,0 +1,11 @@
+function sum(nums) {
+    let num1 = Number(nums[0]);
+    let num2 = Number(nums[1]);
+
+    let result = num1 + num2;
+
+    console.log(result);
+}
+
+sum([10, 20]);
+sum([66, 4]);
