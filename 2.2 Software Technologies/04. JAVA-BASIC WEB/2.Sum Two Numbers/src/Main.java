57a5c6f8577ff08bf5ff8ccae97ea22e65058960
Viktor Levin <viktortus@yahoo.com>
2017-09-26 14:51:29 +0200

    task added

---
+++ 2.2 Software Technologies/04. JAVA-BASIC WEB/2.Sum Two Numbers/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1 = Double.parseDouble(scanner.nextLine());
+        double num2 = Double.parseDouble(scanner.nextLine());
+
+        double sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
