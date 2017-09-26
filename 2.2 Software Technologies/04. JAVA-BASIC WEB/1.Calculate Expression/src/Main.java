720b4d88875784d0f9c4c86636930a41dde4e534
Viktor Levin <viktortus@yahoo.com>
2017-09-26 14:40:53 +0200

    task added

---
+++ 2.2 Software Technologies/04. JAVA-BASIC WEB/1.Calculate Expression/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double val = (30 + 21) * 1.0 / 2.0 * (35 - 12 - 1.0 / 2.0);
+        double sqrVal = Math.pow(val, 2);
+        System.out.println(sqrVal);
+    }
+}
