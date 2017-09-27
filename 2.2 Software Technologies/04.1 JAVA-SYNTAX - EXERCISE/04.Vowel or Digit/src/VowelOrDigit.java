e208e80da2d8e46da57bca8ebb39a0b6eb22b5c5
Viktor Levin <viktortus@yahoo.com>
2017-09-27 14:17:59 +0200

    code modification

--- 2.2 Software Technologies/04.1 JAVA-SYNTAX - EXERCISE/04.Vowel or Digit/src/VowelOrDigit.java
+++ 2.2 Software Technologies/04.1 JAVA-SYNTAX - EXERCISE/04.Vowel or Digit/src/VowelOrDigit.java
@@ -18,23 +18,14 @@ public class VowelOrDigit {
     private static boolean isVowel(String input) {
         switch (input) {
             case "A":
-                return true;
             case "a":
-                return true;
             case "E":
-                return true;
             case "e":
-                return true;
             case "I":
-                return true;
             case "i":
-                return true;
             case "O":
-                return true;
             case "o":
-                return true;
             case "U":
-                return true;
             case "u":
                 return true;
             default:
