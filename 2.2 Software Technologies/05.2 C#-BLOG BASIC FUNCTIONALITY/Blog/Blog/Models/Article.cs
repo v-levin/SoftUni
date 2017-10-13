c8c1f81d8a4a77fc6358e4e63826481b9ccbeb05
Viktor Levin <viktortus@yahoo.com>
2017-10-13 23:26:46 +0200

    task added

---
+++ 2.2 Software Technologies/05.2 C#-BLOG BASIC FUNCTIONALITY/Blog/Blog/Models/Article.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+
+        public Article()
+        {
+            this.DateAdded = DateTime.Now;
+        }
+
+        public bool IsAuthor(string name)
+        {
+            return this.Author.UserName.Equals(name);
+        }
+    }
+}
