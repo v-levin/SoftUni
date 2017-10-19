1e0eb5a0e393165836f5ae17cd106006d7396b28
Viktor Levin <viktortus@yahoo.com>
2017-10-20 01:40:25 +0200

    task added

---
+++ 3.1.1 C# Advanced/01. STACKS AND QUEUES/1. Reverse Strings/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.Reverse_Strings
+{
+    public class ReverseString
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
