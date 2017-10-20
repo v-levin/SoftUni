1a8036d486b621688d0de7d304c7f5711859ae10
Viktor Levin <viktortus@yahoo.com>
2017-10-20 21:28:49 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/03.MaximumElement/MaximumElement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumElement
+{
+    public class MaximumElement
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new Stack<int>();
+            var maxNumbers = new Stack<int>();
+            var maxValue = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                var type = input[0];
+
+                switch (type)
+                {
+                    case 1:
+                        var number = input[1];
+                        numbers.Push(number);
+
+                        if (maxValue < number)
+                        {
+                            maxValue = number;
+                            maxNumbers.Push(maxValue);
+                        }
+
+                        break;
+                    case 2:
+                        if (numbers.Pop() == maxValue)
+                        {
+                            maxNumbers.Pop();
+
+                            if (maxNumbers.Count != 0)
+                            {
+                                maxValue = maxNumbers.Peek();
+                            }
+                            else
+                            {
+                                maxValue = int.MinValue;
+                            }
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine(maxValue);
+                        break;
+                }
+            }
+        }
+    }
+}
