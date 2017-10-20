cd4d261ce0ae44a3e4398f896b292271bb5300db
Viktor Levin <viktortus@yahoo.com>
2017-10-20 14:33:29 +0200

    task added

---
+++ 3.1.1 C# Advanced/01. STACKS AND QUEUES/6.MathPotato/MathPotato.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.MathPotato
+{
+    public class MathPotato
+    {
+        public static void Main()
+        {
+            var children = Console.ReadLine().Split(' ');
+            var number = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>(children);
+
+            int cycle = 1;
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i < number; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                if (IsPrime(cycle))
+                {
+                    Console.WriteLine($"Prime {queue.Peek()}");
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {queue.Dequeue()}");
+                }
+
+                cycle++;
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
