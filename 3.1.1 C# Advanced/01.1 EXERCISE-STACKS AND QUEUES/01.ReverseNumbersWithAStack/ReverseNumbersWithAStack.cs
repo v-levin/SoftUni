b5741c25704e99d311ba7d3540c7a9f50ab7e0b9
Viktor Levin <viktortus@yahoo.com>
2017-10-20 16:36:51 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/01.ReverseNumbersWithAStack/ReverseNumbersWithAStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseNumbersWithAStack
+{
+    public class ReverseNumbersWithAStack
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var number = 0;
+                if (int.TryParse(input[i], out number))
+                {
+                    stack.Push(number);
+                }
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write($"{stack.Pop()} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
