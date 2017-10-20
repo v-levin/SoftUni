7494c324e240c6bb9bfa24b031f555760db0572f
Viktor Levin <viktortus@yahoo.com>
2017-10-20 14:08:37 +0200

    task added

---
+++ 3.1.1 C# Advanced/01. STACKS AND QUEUES/5.HotPotato/HotPotato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.HotPotato
+{
+    public class HotPotato
+    {
+        public static void Main()
+        {
+            var children = Console.ReadLine().Split(' ');
+            var number = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>(children);
+
+            while (queue.Count != 1)
+            {
+                for (int i = 1; i < number; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+
+        }
+    }
+}
