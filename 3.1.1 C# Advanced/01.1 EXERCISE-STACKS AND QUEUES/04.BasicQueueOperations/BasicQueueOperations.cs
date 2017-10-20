17b16fd149166cceb3819b44fbc8a52853066d0a
Viktor Levin <viktortus@yahoo.com>
2017-10-20 23:51:27 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/04.BasicQueueOperations/BasicQueueOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BasicQueueOperations
+{
+    public class BasicQueueOperations
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var n = int.Parse(input[0]);
+            var s = int.Parse(input[1]);
+            var x = int.Parse(input[2]);
+
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var queue = new Queue<int>(); 
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
