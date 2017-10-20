0e98acffd1441559dc67cbc9eb34b5e5734ddf0d
Viktor Levin <viktortus@yahoo.com>
2017-10-20 13:08:56 +0200

    task added

---
+++ 3.1.1 C# Advanced/01. STACKS AND QUEUES/3.DecimalToBinaryConverter/DecimalToBinaryConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.DecimalToBinaryConverter
+{
+    public class DecimalToBinaryConverter
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                while (number != 0)
+                {
+                    var reminder = number % 2;
+
+                    stack.Push(reminder);
+
+                    number /= 2;
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
