8f2c1a25c44b7e6fa6ac0757a28a1bc8fb65d4f1
Viktor Levin <viktortus@yahoo.com>
2017-10-20 17:17:15 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/02.BasicStackOperations/BasicStackOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicStackOperations
+{
+    public class BasicStackOperations
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var n = int.Parse(input[0]);
+            var s = int.Parse(input[1]);
+            var x = int.Parse(input[2]);
+
+            var integers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(integers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
