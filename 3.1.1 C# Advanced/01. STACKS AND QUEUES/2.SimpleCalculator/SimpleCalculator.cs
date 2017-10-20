02358255bfd55e1b51ee449c2807581bdb2a0497
Viktor Levin <viktortus@yahoo.com>
2017-10-20 12:46:24 +0200

    task added

---
+++ 3.1.1 C# Advanced/01. STACKS AND QUEUES/2.SimpleCalculator/SimpleCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.SimpleCalculator
+{
+    public class SimpleCalculator
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var inputParams = input.Split(' ');
+            var stack = new Stack<string>(inputParams.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var firstNumber = int.Parse(stack.Pop());
+                var op = stack.Pop();
+                var secondNumber = int.Parse(stack.Pop());
+
+                if (op == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+                else
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
