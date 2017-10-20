9eb373501c696541fc92b06f11dd9139a3c2a43d
Viktor Levin <viktortus@yahoo.com>
2017-10-20 13:42:48 +0200

    task added

---
+++ 3.1.1 C# Advanced/01. STACKS AND QUEUES/4.MatchingBrackets/MatchingBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    public class MatchingBrackets
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    var startIndex = stack.Pop();
+                    var subExpression = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+        }
+    }
+}
