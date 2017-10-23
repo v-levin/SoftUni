4fb5feadf21130c83fcb4efe2060406e356323c3
Viktor Levin <viktortus@yahoo.com>
2017-10-23 16:53:23 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/07.BalancedParentheses/BalancedParentheses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.BalancedParentheses
+{
+    public class BalancedParentheses
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var openedParentheses = new Stack<char>();
+            var openCases = new char[] { '{', '(', '[' };
+
+            foreach (var character in inputLine)
+            {
+                if (openCases.Contains(character))
+                {
+                    openedParentheses.Push(character);
+                }
+                else
+                {
+                    if (openedParentheses.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+
+                    switch (character)
+                    {
+                        case '}':
+                            if (openedParentheses.Pop() != '{')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+
+                        case ')':
+                            if (openedParentheses.Pop() != '(')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+
+                        case ']':
+                            if (openedParentheses.Pop() != '[')
+                            {
+                                Console.WriteLine("NO");
+                                return;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
