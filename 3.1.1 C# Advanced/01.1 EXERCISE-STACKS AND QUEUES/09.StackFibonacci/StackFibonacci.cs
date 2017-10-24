f28e0ef392c475aa55d9f03bc70af479f4dc5169
Viktor Levin <viktortus@yahoo.com>
2017-10-24 15:18:22 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/09.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.StackFibonacci
+{
+    public class StackFibonacci
+    {
+        public static void Main()
+        {
+            var fibonacciStack = new Stack<long>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            fibonacciStack.Push(0);
+            var firstNumber = fibonacciStack.Peek();
+            fibonacciStack.Push(1);
+            var secondNumber = fibonacciStack.Peek();
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                fibonacciStack.Push(firstNumber + secondNumber);
+
+                firstNumber = secondNumber;
+                secondNumber = fibonacciStack.Peek();
+            }
+
+            Console.WriteLine(fibonacciStack.Peek());
+        }
+    }
+}
