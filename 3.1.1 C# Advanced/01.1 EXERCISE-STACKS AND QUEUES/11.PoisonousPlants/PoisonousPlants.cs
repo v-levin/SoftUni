54d8fa4ae5fed49cd6a7e98309862d8ebf8d38fa
Viktor Levin <viktortus@yahoo.com>
2017-10-24 20:45:35 +0200

    task added

---
+++ 3.1.1 C# Advanced/01.1 EXERCISE-STACKS AND QUEUES/11.PoisonousPlants/PoisonousPlants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.PoisonousPlants
+{
+    public class PoisonousPlants
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var plants = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var days = new int[n];
+            var plantsIndexStack = new Stack<int>();
+            plantsIndexStack.Push(0);
+
+            for (int i = 1; i < plants.Length; i++)
+            {
+                var maxDays = 0;
+
+                while (plantsIndexStack.Count > 0 && plants[plantsIndexStack.Peek()] >= plants[i])
+                {
+                    maxDays = Math.Max(maxDays, days[plantsIndexStack.Pop()]);
+                }
+
+                if (plantsIndexStack.Count > 0)
+                {
+                    days[i] = maxDays + 1;
+                }
+
+                plantsIndexStack.Push(i);
+            }
+
+            Console.WriteLine(days.Max());
+        }
+    }
+}
