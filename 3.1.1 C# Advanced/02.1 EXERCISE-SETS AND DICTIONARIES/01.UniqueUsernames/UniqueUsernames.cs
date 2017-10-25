1def94137050a501b395484f0373d5fbe4a24983
Viktor Levin <viktortus@yahoo.com>
2017-10-25 18:23:45 +0200

    task added

---
+++ 3.1.1 C# Advanced/02.1 EXERCISE-SETS AND DICTIONARIES/01.UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var username = Console.ReadLine();
+
+                usernames.Add(username);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}
