ecbd643852c16c6f34f8347dbcea5a5bdc92078f
Viktor Levin <viktortus@yahoo.com>
2017-10-25 20:38:41 +0200

    task added

---
+++ 3.1.1 C# Advanced/02.1 EXERCISE-SETS AND DICTIONARIES/03.PeriodicTable/PeriodicTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PeriodicTable
+{
+    public class PeriodicTable
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", periodicTable));
+        }
+    }
+}
