97c31178da156adf45acb7388c03d80a0896ac9b
Viktor Levin <viktortus@yahoo.com>
2017-10-25 17:50:21 +0200

    task added

---
+++ 3.1.1 C# Advanced/02. SETS AND DICTIONARIES/3.CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.CountSameValuesInArray
+{
+    public class CountSameValuesInArray
+    {
+        public static void Main()
+        {
+            var tokens = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            var valueOccurances = new SortedDictionary<double, int>();
+
+            foreach (var number in tokens)
+            {
+                if (!valueOccurances.ContainsKey(number))
+                {
+                    valueOccurances[number] = 1;
+                    continue;
+                }
+
+                valueOccurances[number]++;
+            }
+
+            foreach (var kvp in valueOccurances)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
