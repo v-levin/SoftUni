9d1d0104fb4f8cd3b48c55343e85249638f4ec5f
Viktor Levin <viktortus@yahoo.com>
2017-10-25 16:34:31 +0200

    task added

---
+++ 3.1.1 C# Advanced/02. SETS AND DICTIONARIES/2.SoftUniParty/SoftUniParty.cs
@@ -0,0 +1,34 @@
+﻿namespace _2.SoftUniParty
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SoftUniParty
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var questsReservations = new SortedSet<string>();
+
+            while (!input.Equals("PARTY"))
+            {
+                questsReservations.Add(input);
+
+                input = Console.ReadLine();
+            }
+
+            while (!input.Equals("END"))
+            {
+                if (questsReservations.Contains(input))
+                {
+                    questsReservations.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(questsReservations.Count);
+            Console.WriteLine(string.Join(Environment.NewLine, questsReservations));
+        }
+    }
+}
