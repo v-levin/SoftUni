d1be7fc04cf945e01ef686216293c65dbd569efe
Viktor Levin <viktortus@yahoo.com>
2017-10-25 18:07:58 +0200

    task added

---
+++ 3.1.1 C# Advanced/02. SETS AND DICTIONARIES/4.AcademyGraduation/AcademyGraduation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.AcademyGraduation
+{
+    public class AcademyGraduation
+    {
+        public static void Main()
+        {
+            var numberOfStudents = int.Parse(Console.ReadLine());
+            var students = new SortedDictionary<string, double[]>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                var studentName = Console.ReadLine();
+                var studentScores = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+                students.Add(studentName, studentScores);
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key} is graduated with {student.Value.Average()}");
+            }
+        }
+    }
+}
