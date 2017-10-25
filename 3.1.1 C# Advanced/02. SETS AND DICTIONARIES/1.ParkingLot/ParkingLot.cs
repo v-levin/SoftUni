e74ac3d4530cc2784dd9fbfe5ea5e48284137ccf
Viktor Levin <viktortus@yahoo.com>
2017-10-25 14:42:28 +0200

    task added

---
+++ 3.1.1 C# Advanced/02. SETS AND DICTIONARIES/1.ParkingLot/ParkingLot.cs
@@ -0,0 +1,44 @@
+﻿namespace _1.ParkingLot
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ParkingLot
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var parkedCars = new SortedSet<string>(); 
+
+            while (!input.Equals("END"))
+            {
+                var inputParams = input.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var direcrion = inputParams[0];
+                var carNumber = inputParams[1];
+
+                if (direcrion.Equals("IN"))
+                {
+                    parkedCars.Add(carNumber);
+                }
+                else if (direcrion.Equals("OUT"))
+                {
+                    if (parkedCars.Contains(carNumber))
+                    {
+                        parkedCars.Remove(carNumber);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parkedCars.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, parkedCars));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
