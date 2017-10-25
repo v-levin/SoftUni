177a12332b75c48554d9144d5e446eed0e502e53
Viktor Levin <viktortus@yahoo.com>
2017-10-25 19:46:05 +0200

    task added

---
+++ 3.1.1 C# Advanced/02.1 EXERCISE-SETS AND DICTIONARIES/02.SetsOfElements/SetsOfElements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    public class SetsOfElements
+    {
+        public static void Main()
+        {
+            var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var n = tokens[0];
+            var m = tokens[1];
+
+            var nSet = new HashSet<int>();
+            var mSet = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                nSet.Add(number);
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                mSet.Add(number);
+            }
+
+            Console.WriteLine(string.Join(" ", nSet.Intersect(mSet)));
+        }
+    }
+}
