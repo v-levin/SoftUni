b03fa8a5011c9d87cd22327499d6c1cd896e16c4
Viktor Levin <viktortus@yahoo.com>
2017-10-25 20:47:08 +0200

    task added

---
+++ 3.1.1 C# Advanced/02.1 EXERCISE-SETS AND DICTIONARIES/04.CountSymbols/CountSymbols.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.CountSymbols
+{
+    public class CountSymbols
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var countSymbols = new SortedDictionary<char, int>();
+
+            foreach (var character in text)
+            {
+                if (!countSymbols.ContainsKey(character))
+                {
+                    countSymbols.Add(character, 1);
+                    continue;
+                }
+
+                countSymbols[character]++;
+            }
+
+            foreach (var kvp in countSymbols)
+            {
+                var symbol = kvp.Key;
+                var count = kvp.Value;
+
+                Console.WriteLine($"{symbol}: {count} time/s");
+            }
+        }
+    }
+}
