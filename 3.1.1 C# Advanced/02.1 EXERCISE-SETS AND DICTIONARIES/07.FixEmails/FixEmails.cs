eabaa6b7420203b29d85df06de0e2058eec9a723
Viktor Levin <viktortus@yahoo.com>
2017-10-26 13:35:45 +0200

    task added

---
+++ 3.1.1 C# Advanced/02.1 EXERCISE-SETS AND DICTIONARIES/07.FixEmails/FixEmails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.FixEmails
+{
+    public class FixEmails
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var emails = new Dictionary<string, string>();
+
+            while (!input.Equals("stop"))
+            {
+                var name = input;
+                var email = Console.ReadLine();
+                var emailParams = email.Split('.');
+                var domain = emailParams[emailParams.Length - 1];
+
+                if (!domain.Equals("us") && !domain.Equals("uk"))
+                {
+                    emails.Add(name, email);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var kvp in emails)
+            {
+                var name = kvp.Key;
+                var email = kvp.Value;
+
+                Console.WriteLine($"{name} -> {email}");
+            }
+        }
+    }
+}
