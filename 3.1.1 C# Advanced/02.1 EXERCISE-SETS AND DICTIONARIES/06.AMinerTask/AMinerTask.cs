639836628a9383a77b94df0f35e6bd392fb2e2aa
Viktor Levin <viktortus@yahoo.com>
2017-10-26 12:08:45 +0200

    task added

---
+++ 3.1.1 C# Advanced/02.1 EXERCISE-SETS AND DICTIONARIES/06.AMinerTask/AMinerTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.AMinerTask
+{
+    public class AMinerTask
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var minerTasks = new Dictionary<string, int>();
+
+            while (!input.Equals("stop"))
+            {
+                var resource = input;
+                var quantity = int.Parse(Console.ReadLine());
+
+                if (minerTasks.ContainsKey(input))
+                {
+                    minerTasks[input] += quantity;
+                }
+                else
+                {
+                    minerTasks.Add(input, quantity);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var kvp in minerTasks)
+            {
+                var minerResource = kvp.Key;
+                var quantity = kvp.Value;
+
+                Console.WriteLine($"{minerResource} -> {quantity}");
+            }
+        }
+    }
+}
