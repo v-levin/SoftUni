817e477543d672eeecc89ac2b244518180bc841e
Viktor Levin <viktortus@yahoo.com>
2017-11-01 14:20:51 +0100

    third lab of project added

--- 3.1.1 C# Advanced/02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW/BashSoft/BashSoft/BashSoftProgram.cs
+++ 3.1.1 C# Advanced/02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW/BashSoft/BashSoft/BashSoftProgram.cs
@@ -11,10 +11,16 @@ namespace BashSoft
         static void Main()
         {
             //IOManager.TraverseDirectory(@"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft");
+            //IOManager.TraverseDirectory();
+            IOManager.TraverseDirectory(0);
 
-            StudentsRepository.InitializeData();
-            StudentsRepository.GetAllStudentsFromCourse("Unity");
+            //StudentsRepository.InitializeData();
+            //StudentsRepository.GetAllStudentsFromCourse("Unity");
             //StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
+
+            //Tester.CompareContent(@"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft-FirstWeek-Tasks\03. CSharp-Advanced-Files-And-Directories-Lab\test2.txt", @"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft-FirstWeek-Tasks\03. CSharp-Advanced-Files-And-Directories-Lab\test3.txt");
+
+            //IOManager.CreateDirectoryInCurrentFolder("pesho");
         }
     }
 }
--- 3.1.1 C# Advanced/02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW/BashSoft/BashSoft/Static data/ExceptionMessages.cs
+++ 3.1.1 C# Advanced/02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW/BashSoft/BashSoft/Static data/ExceptionMessages.cs
@@ -17,5 +17,7 @@ namespace BashSoft
         public const string InexistingCourseInDataBase = "The course you are trying to get does not exist in the data base!";
 
         public const string InexistingStudentInDataBase = "The user name for the student you are trying to get does not exist!";
+
+        public const string InvalidPath = "The folder/file you are trying to access at the current address, does not exist.";
     }
 }
