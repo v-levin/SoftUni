22777ee9db177fd7e5a6731b9d8ae13aa0be6df2
Viktor Levin <viktortus@yahoo.com>
2017-11-03 12:37:21 +0100

    task added

---
+++ 3.1.1 C# Advanced/03. MATRICES/1.SumMatrixElements/SumMatrixElements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SumMatrixElements
+{
+    public class SumMatrixElements
+    {
+        public static void Main()
+        {
+            var matrixSizes = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            var rows = int.Parse(matrixSizes[0]);
+            var matrix = new int[rows][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            var sum = 0;
+
+            foreach (var num in matrix)
+            {
+                sum += num.Sum();
+            }
+            
+            Console.WriteLine(matrix.Length);
+            Console.WriteLine(matrix[0].Length);
+            Console.WriteLine(sum);
+        }
+    }
+}
