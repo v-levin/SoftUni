ca604c71bba35a1bf767561ba7ff96bb0d090f2f
Viktor Levin <viktortus@yahoo.com>
2017-11-03 18:18:47 +0100

    task added

---
+++ 3.1.1 C# Advanced/03. MATRICES/4.PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4.PascalTriangle
+{
+    public class PascalTriangle
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var pascalTriangle = new long[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                pascalTriangle[row][0] = 1;
+                pascalTriangle[row][row] = 1;
+
+                for (int col = 1; col < pascalTriangle[row].Length - 1; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+
+                Console.WriteLine(string.Join(" ", pascalTriangle[row]));
+            }
+        }
+    }
+}
