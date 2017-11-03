1ac7c42f930dc727efbad2c44726730ab01e0edd
Viktor Levin <viktortus@yahoo.com>
2017-11-03 14:44:40 +0100

    task added

---
+++ 3.1.1 C# Advanced/03. MATRICES/3.GroupNumbers/GroupNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.GroupNumbers
+{
+    public class GroupNumbers
+    {
+        public static void Main()
+        {
+            var matrix = new int[3][];
+
+            var numbers = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            matrix[0] = numbers.Where(n => Math.Abs(n) % 3 == 0).ToArray();
+            matrix[1] = numbers.Where(n => Math.Abs(n) % 3 == 1).ToArray();
+            matrix[2] = numbers.Where(n => Math.Abs(n) % 3 == 2).ToArray();
+
+            Console.WriteLine($"{string.Join(" ", matrix[0])}\n{string.Join(" ", matrix[1])}\n{string.Join(" ", matrix[2])}");
+        }
+    }
+}
