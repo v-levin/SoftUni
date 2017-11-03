b82986082fa80c6319c1c17948bdd81a0096421c
Viktor Levin <viktortus@yahoo.com>
2017-11-03 21:36:02 +0100

    task added

---
+++ 3.1.1 C# Advanced/03.1 EXERCISE-MATRICES/02.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.DiagonalDifference
+{
+    public class DiagonalDifference
+    {
+        public static void Main()
+        {
+            var matrixSize = int.Parse(Console.ReadLine());
+
+            var matrix = new int[matrixSize][];
+            var primaryDiagonalSum = 0;
+            var secondaryDiagonalSum = 0;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                var currentRow = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                matrix[i] = currentRow;
+
+                primaryDiagonalSum += matrix[i][i];
+                secondaryDiagonalSum += matrix[i][matrixSize - 1 - i];
+            }
+
+            var difference = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+
+            Console.WriteLine(difference);
+        }
+    }
+}
