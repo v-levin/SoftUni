a2a8bfb76e9e2a4d830dd89c5d39322cbb26b16b
Viktor Levin <viktortus@yahoo.com>
2017-11-03 13:30:25 +0100

    task added

---
+++ 3.1.1 C# Advanced/03. MATRICES/2.MaximumSumOf2x2Submatrix/MaximumSumOf2x2Submatrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.MaximumSumOf2x2Submatrix
+{
+    public class MaximumSumOf2x2Submatrix
+    {
+        public static void Main()
+        {
+            var matrixSizes = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            var rows = int.Parse(matrixSizes[0]);
+            var matrix = new int[rows][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            var maxSquareRow = 0;
+            var maxSquareCol = 0;
+            var maxSum = int.MinValue;
+
+            for (int row = 0; row < matrix.Length - 1; row++)
+            {
+                for (int col = 0; col < matrix[row].Length - 1; col++)
+                {
+                    var currentSum = matrix[row][col] + matrix[row][col + 1] + matrix[row + 1][col] + matrix[row + 1][col + 1];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxSquareRow = row;
+                        maxSquareCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxSquareRow][maxSquareCol]} {matrix[maxSquareRow][maxSquareCol + 1]}\n" +
+                $"{matrix[maxSquareRow + 1][maxSquareCol]} {matrix[maxSquareRow + 1][maxSquareCol + 1]}\n" +
+                $"{maxSum}");
+        }
+    }
+}
