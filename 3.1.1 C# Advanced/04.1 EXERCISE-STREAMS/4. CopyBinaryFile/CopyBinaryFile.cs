8d23e1860f5120c837c600c29b5f108689109215
Viktor Levin <viktortus@yahoo.com>
2017-11-16 20:28:39 +0100

    task added

---
+++ 3.1.1 C# Advanced/04.1 EXERCISE-STREAMS/4. CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace _4.CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        const string SheepImagePath = "../../sheep.jpg";
+        const string DestinationPath = "../../result.jpg";
+
+        public static void Main()
+        {
+            using (var source = new FileStream(SheepImagePath, FileMode.Open))
+            using (var destination = new FileStream(DestinationPath, FileMode.Create))
+            {
+                double fileLength = source.Length;
+                var buffer = new byte[4096];
+                while (true)
+                {
+                    var readBytes = source.Read(buffer, 0, buffer.Length);
+                    if (readBytes == 0)
+                    {
+                        break;
+                    }
+
+                    destination.Write(buffer, 0, readBytes);
+                }
+            }
+        }
+    }
+}
