435464e2b12843b08a6afdd316ea1a1b0fcd364d
Viktor Levin <viktortus@yahoo.com>
2017-11-16 18:13:23 +0100

    task added

---
+++ 3.1.1 C# Advanced/04.1 EXERCISE-STREAMS/3. WordCount/WordCount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _3.WordCount
+{
+    public class WordCount
+    {
+        public static void Main()
+        {
+            var result = new Dictionary<string, int>();
+
+            using (var wordReader = new StreamReader("../../words.txt"))
+            {
+                var word = string.Empty;
+                while ((word = wordReader.ReadLine()) != null)
+                {
+                    word = word.ToLower();
+                    result[word] = 0;
+
+                    using (var textReader = new StreamReader("../../text.txt"))
+                    {
+                        var text = string.Empty;
+                        while ((text = textReader.ReadLine()) != null)
+                        {
+                            var wordsInLine = text.ToLower().Split(new[] { ' ', '-', '?', '!', '.', ',', ':', '\'', '"', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                            foreach (var wordInLine in wordsInLine)
+                            {
+                                if (word == wordInLine)
+                                {
+                                    result[word]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter("../../result.txt"))
+            {
+                foreach (var word in result.OrderByDescending(w => w.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            }
+        }
+    }
+}
