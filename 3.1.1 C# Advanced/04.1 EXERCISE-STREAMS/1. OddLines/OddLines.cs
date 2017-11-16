9b7f58ca18037ce5ae3c97d4ba883a947f0cc991
Viktor Levin <viktortus@yahoo.com>
2017-11-16 17:26:57 +0100

    task added

---
+++ 3.1.1 C# Advanced/04.1 EXERCISE-STREAMS/1. OddLines/OddLines.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _1.OddLines
+{
+    public class OddLines
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader("../../test.txt"))
+            {
+                var readLine = string.Empty;
+                var counter = 1;
+                while ((readLine = reader.ReadLine()) != null)
+                {
+                    if (counter % 2 == 1)
+                    {
+                        Console.WriteLine(readLine);
+                    }
+
+                    counter++;
+                }
+            }
+        }
+    }
+}
