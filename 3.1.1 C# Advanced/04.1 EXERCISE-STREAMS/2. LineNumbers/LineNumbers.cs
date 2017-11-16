796c489fdd13ea7f30d1d3445c8d1b8854ecd42e
Viktor Levin <viktortus@yahoo.com>
2017-11-16 17:34:49 +0100

    task added

---
+++ 3.1.1 C# Advanced/04.1 EXERCISE-STREAMS/2. LineNumbers/LineNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _2.LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            Console.Write("Choose a file path: ");
+            var filePath = Console.ReadLine();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var writer = new StreamWriter("../../result.txt"))
+                {
+                    var readLine = string.Empty;
+                    var counter = 1;
+                    while ((readLine = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter} {readLine}");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
