8baad8861cd7f6b8294b2aeb02a44bbe7cbba698
Viktor Levin <viktortus@yahoo.com>
2017-11-22 20:00:45 +0100

    task added

---
+++ 3.1.1 C# Advanced/04.1 EXERCISE-STREAMS/9. HTTPServer/HTTPServer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _9.HTTPServer
+{
+    public class HTTPServer
+    {
+        private const int Port = 8888;
+        private const string HtmlFilesPath = "../../pages";
+
+        public static void Main()
+        {
+            var localAddress = IPAddress.Parse("127.0.0.1");
+            var server = new TcpListener(localAddress, Port);
+            server.Start();
+
+            Console.WriteLine($"Waiting for a connection on {localAddress}:{Port} ... ");
+
+            while (true)
+            {
+                using (var stream = server.AcceptTcpClient().GetStream())
+                {
+                    var request = new byte[4096];
+                    var readBytes = stream.Read(request, 0, request.Length);
+                    var requestDetails = Encoding.UTF8.GetString(request, 0, request.Length);
+                    Console.WriteLine(requestDetails);
+
+                    var requestFirstLine = requestDetails.Substring(0, requestDetails.IndexOf(Environment.NewLine)).Split();
+                    var url = requestFirstLine[1];
+                    var headerStatusLine = $"{requestFirstLine[2]} 200 OK";
+
+                    var requestedPage = string.Empty;
+
+                    if (url.Equals("/"))
+                    {
+                        requestedPage = $"{HtmlFilesPath}/index.html";
+                    }
+                    else
+                    {
+                        requestedPage = $"{HtmlFilesPath}{url.Substring(url.IndexOf('/'))}";
+
+                        if (!requestedPage.EndsWith(".html"))
+                        {
+                            requestedPage += ".html";
+                        }
+
+                        if (!File.Exists(requestedPage))
+                        {
+                            requestedPage = $"{HtmlFilesPath}/error.html";
+                        }
+                        else
+                        {
+                            headerStatusLine = "HTTP/1.0 404 Not Found";
+                        }
+                    }
+
+                    var responseHeader = new StringBuilder();
+                    responseHeader.Append($"{headerStatusLine}{Environment.NewLine}");
+                    responseHeader.Append($"Accept-Ranges: bytes{Environment.NewLine}");
+
+                    var responseMessage = new StringBuilder();
+
+                    using (var reader = new StreamReader(requestedPage))
+                    {
+                        var line = reader.ReadLine();
+                        while (line != null)
+                        {
+                            responseMessage.Append(line);
+                            line = reader.ReadLine();
+                        }
+                    }
+
+                    if (requestedPage.EndsWith("info.html"))
+                    {
+                        responseMessage
+                            .Replace("{0}", DateTime.Now.ToString("ddd, MMM d, yyyy"))
+                            .Replace("{1}", Environment.ProcessorCount.ToString());
+                    }
+
+                    var contentLength = Encoding.UTF8.GetBytes(responseMessage.ToString()).Length;
+
+                    responseHeader.Append($"ContentLength: {contentLength}{Environment.NewLine}");
+                    responseHeader.Append($"Connection: close{Environment.NewLine}");
+                    responseHeader.Append($"Content-Type: text/html{Environment.NewLine}");
+                    responseHeader.Append(Environment.NewLine);
+
+                    responseMessage.Insert(0, responseHeader);
+
+                    var responseBytes = Encoding.UTF8.GetBytes(responseMessage.ToString());
+
+                    stream.Write(responseBytes, 0, responseBytes.Length);
+                }
+            }
+        }
+    }
+}
