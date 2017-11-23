89cd612c0d1154d203e74ed84ae59e8f71edc951
Viktor Levin <viktortus@yahoo.com>
2017-11-23 20:11:34 +0100

    task added

---
+++ 3.1.1 C# Advanced/05. MANUAL STRING PROCESSING/3.ParseTags/ParseTags.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3.ParseTags
+{
+    public class ParseTags
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var openTag = "<upcase>";
+            var closeTag = "</upcase>";
+
+            var startIndex = text.IndexOf(openTag);
+            while (startIndex != -1)
+            {
+                var endIndex = text.IndexOf(closeTag);
+
+                if (endIndex == -1)
+                {
+                    break;
+                }
+
+                var toBeReplaced = text.Substring(startIndex, endIndex + closeTag.Length - startIndex);
+                var upperCase = toBeReplaced.Replace(openTag, string.Empty).Replace(closeTag, string.Empty).ToUpper();
+
+                text = text.Replace(toBeReplaced, upperCase);
+
+                startIndex = text.IndexOf(openTag);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
