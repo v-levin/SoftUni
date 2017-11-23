2f1c58192bf4deda58f552e5fde681e42ae95f45
Viktor Levin <viktortus@yahoo.com>
2017-11-23 18:04:59 +0100

    task added

---
+++ 3.1.1 C# Advanced/05. MANUAL STRING PROCESSING/2.ParseURLs/ParseURLs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.ParseURLs
+{
+    public class ParseURLs
+    {
+        public static void Main()
+        {
+            var url = Console.ReadLine();
+            var separator = "://";
+
+            var urlTokens = url.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (urlTokens.Length != 2 || !urlTokens[1].Contains('/'))
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+
+            var protocol = urlTokens[0];
+            var indexResources = urlTokens[1].IndexOf('/');
+            var server = urlTokens[1].Substring(0, indexResources);
+            var resources = urlTokens[1].Substring(indexResources + 1);
+
+            Console.WriteLine($"Protocol = {protocol}\nServer = {server}\nResources = {resources}");
+        }
+    }
+}
