a80a77096b0f236ca0485b86f916f0a85b810425
Viktor Levin <viktortus@yahoo.com>
2017-11-24 20:26:20 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/02.StringLength/StringLength.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.StringLength
+{
+    public class StringLength
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var maxChars = 20;
+
+            if (text.Length > maxChars)
+            {
+                text = text.Substring(0, maxChars);
+            }
+            else if (text.Length < maxChars)
+            {
+                text = string.Concat(text, new string('*', maxChars - text.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
