1ac64e1a7f378e7d35a692ddc6cfa2aed383e7f6
Viktor Levin <viktortus@yahoo.com>
2017-11-24 14:11:17 +0100

    task added

---
+++ 3.1.1 C# Advanced/05. MANUAL STRING PROCESSING/4.SpecialWords/SpecialWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4.SpecialWords
+{
+    public class SpecialWords
+    {
+        public static void Main()
+        {
+            var specialWords = Console.ReadLine().Split(' ');
+            var text = Console.ReadLine().Split(new[] { '(', ')', '[', ']', '<', '>', ',', '-', '!', '?', ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var specialWord in specialWords)
+            {
+                var counter = 0;
+
+                foreach (var word in text)
+                {
+                    if (specialWord == word)
+                    {
+                        counter++;
+                    }
+                }
+
+                Console.WriteLine($"{specialWord} - {counter}");
+            }
+        }
+    }
+}
