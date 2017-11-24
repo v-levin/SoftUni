bb76c822b32c3d2fa670e8874cc9735677f4e792
Viktor Levin <viktortus@yahoo.com>
2017-11-24 20:18:00 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/01.ReverseString/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _01.ReverseString
+{
+    public class ReverseString
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var sb = new StringBuilder();
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                sb.Append(text[i]);
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
