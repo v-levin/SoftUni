ccbccaa2e4561cbca4d6bea4f19e453dd783546d
Viktor Levin <viktortus@yahoo.com>
2017-11-24 14:39:12 +0100

    task added

---
+++ 3.1.1 C# Advanced/05. MANUAL STRING PROCESSING/5.ConcatenateStrings/ConcatenateStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _5.ConcatenateStrings
+{
+    public class ConcatenateStrings
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(Console.ReadLine()).Append(" ");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
