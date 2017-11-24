f6f1de10a73794f760fb422e9f914b505f1b4624
Viktor Levin <viktortus@yahoo.com>
2017-11-24 23:54:02 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/05.ConvertFromBaseNToBase10/ConvertFromBaseNToBase10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05.ConvertFromBaseNToBase10
+{
+    public class ConvertFromBaseNToBase10
+    {
+        public static void Main()
+        {
+            var inputNums = Console.ReadLine().Split();
+            var baseN = int.Parse(inputNums[0]);
+            var number = BigInteger.Parse(inputNums[1]);
+
+            var result = ConvertFromBaseNTo10(number, baseN);
+
+            Console.WriteLine(result);
+        }
+
+        private static BigInteger ConvertFromBaseNTo10(BigInteger number, int baseN)
+        {
+            var result = new BigInteger(0);
+            var numAsString = number.ToString();
+
+            for (int i = 0; i < numAsString.Length; i++)
+            {
+                var num = int.Parse(numAsString[numAsString.Length - 1 - i].ToString());
+                result += num * BigInteger.Pow(baseN, i);
+            }
+
+            return result;
+        }
+    }
+}
