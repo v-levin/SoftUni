8c82d8c416c77cca299caa7adffc99d1646a8903
Viktor Levin <viktortus@yahoo.com>
2017-11-26 22:26:45 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/07.SumBignumbers/SumBignumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.SumBignumbers
+{
+    public class SumBignumbers
+    {
+        public static void Main()
+        {
+            var firstNum = new Stack<char>(Console.ReadLine());
+            var secondNum = new Stack<char>(Console.ReadLine());
+
+            var result = new StringBuilder();
+            var sum = 0;
+            while (firstNum.Count != 0 || secondNum.Count != 0)
+            {
+                sum /= 10;
+                if (firstNum.Count != 0) sum += int.Parse(firstNum.Pop().ToString());
+                if (secondNum.Count != 0) sum += int.Parse(secondNum.Pop().ToString());
+
+                result.Insert(0, sum % 10);
+            }
+
+            result.Insert(0, sum / 10);
+
+            Console.WriteLine(result.ToString().TrimStart('0'));
+        }
+    }
+}
