bf129deac90241b7890b336d308c9fe3accbf4cd
Viktor Levin <viktortus@yahoo.com>
2017-11-26 18:51:30 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/06.CountSubstringOccurrences/CountSubstringOccurrences.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06.CountSubstringOccurrences
+{
+    public class CountSubstringOccurrences
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().ToLower();
+            var searchString = Console.ReadLine().ToLower();
+
+            Console.WriteLine(CountStringOccurences(text, searchString));
+        }
+
+        private static int CountStringOccurences(string text, string searchString)
+        {
+            var count = 0;
+            var indexOfSubstring = text.IndexOf(searchString);
+
+            while (indexOfSubstring != -1)
+            {
+                count++;
+                indexOfSubstring = text.IndexOf(searchString, indexOfSubstring + 1);
+            }
+
+            return count;
+        }
+    }
+}
