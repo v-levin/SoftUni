e6e345d1bc39b3810a7a2109d2bbc8b11f5209de
Viktor Levin <viktortus@yahoo.com>
2017-11-27 18:03:00 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/10.UnicodeCharacters/UnicodeCharacters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _10.UnicodeCharacters
+{
+    public class UnicodeCharacters
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new StringBuilder();
+
+            foreach (var character in input)
+            {
+                var unicode = ((int)character).ToString("X4").ToLower();
+                result.Append($"\\u{unicode}");
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
