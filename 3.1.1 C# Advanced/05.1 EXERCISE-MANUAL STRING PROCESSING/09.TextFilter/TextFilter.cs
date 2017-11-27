0881611c275c01742991b9c31973f308e8d05e63
Viktor Levin <viktortus@yahoo.com>
2017-11-27 17:37:38 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/09.TextFilter/TextFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09.TextFilter
+{
+    public class TextFilter
+    {
+        public static void Main()
+        {
+            var bannedWords = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            var text = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                if (text.Contains(bannedWord))
+                {
+                    text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+                }
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
