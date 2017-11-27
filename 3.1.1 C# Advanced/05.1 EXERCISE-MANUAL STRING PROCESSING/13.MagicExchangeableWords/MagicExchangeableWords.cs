a48e4662e06210a910db78950393f7edfc76e58c
Viktor Levin <viktortus@yahoo.com>
2017-11-28 00:39:17 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/13.MagicExchangeableWords/MagicExchangeableWords.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13.MagicExchangeableWords
+{
+    public class MagicExchangeableWords
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            var firstString = input[0];
+            var secondString = input[1];
+
+            var mappedChars = new Dictionary<char, char>();
+
+            var shorterString = string.Empty;
+            var longerString = string.Empty;
+            if (firstString.Length > secondString.Length)
+            {
+                shorterString = secondString;
+                longerString = firstString;
+            }
+            else if (firstString.Length < secondString.Length)
+            {
+                shorterString = firstString;
+                longerString = secondString;
+            }
+
+            var magicWord = true;
+            for (int i = 0; i < shorterString.Length; i++)
+            {
+                if (!mappedChars.ContainsKey(longerString[i]))
+                {
+                    mappedChars.Add(longerString[i], shorterString[i]);
+                }
+                else if (mappedChars[longerString[i]] != shorterString[i])
+                {
+                    magicWord = false;
+                    break;
+                }
+            }
+
+            if (magicWord)
+            {
+                for (int i = shorterString.Length; i < longerString.Length; i++)
+                {
+                    if (!mappedChars.ContainsKey(longerString[i]))
+                    {
+                        magicWord = false;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(magicWord.ToString().ToLower());
+        }
+    }
+}
