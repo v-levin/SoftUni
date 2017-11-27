01eac9b854a5159bf0f408cf16fd8603d682b1a2
Viktor Levin <viktortus@yahoo.com>
2017-11-27 18:20:50 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/11.Palindromes/Palindromes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.Palindromes
+{
+    public class Palindromes
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().Split(new[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var palindromes = new List<string>();
+
+            var isPalindrom = true;
+            foreach (var word in text)
+            {
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    if (word[i] != word[word.Length - 1 - i])
+                    {
+                        isPalindrom = false;
+                        break;
+                    }
+                }
+
+                if (isPalindrom)
+                {
+                    palindromes.Add(word);
+                }
+
+                isPalindrom = true;
+            }
+
+            Console.WriteLine($"[{string.Join(", ", palindromes.Distinct().OrderBy(w => w))}]");
+        }
+    }
+}
