ce929932b38550873b10932c2fae78adf7585e0e
Viktor Levin <viktortus@yahoo.com>
2017-11-28 14:07:40 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/14.LettersChangeNumbers/LettersChangeNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _14.LettersChangeNumbers
+{
+    public class LettersChangeNumbers
+    {
+        public static void Main()
+        {
+            var strings = Console.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var sum = 0m;
+            foreach (var str in strings)
+            {
+                var firstLetter = str[0];
+                var num = decimal.Parse(str.Substring(1, str.Length - 2));
+                var lastLetter = str[str.Length - 1];
+
+                if (char.IsUpper(firstLetter))
+                {
+                    sum += num / GetIndexInAlphabet(firstLetter);
+                }
+                else
+                {
+                    sum += num * GetIndexInAlphabet(firstLetter);
+                }
+
+                if (char.IsUpper(lastLetter))
+                {
+                    sum -= GetIndexInAlphabet(lastLetter);
+                }
+                else
+                {
+                    sum += GetIndexInAlphabet(lastLetter);
+                }
+            }
+
+            Console.WriteLine($"{sum:F2}");
+        }
+
+        public static int GetIndexInAlphabet(char letter)
+        {
+            return letter % 32;
+        }
+    }
+}
