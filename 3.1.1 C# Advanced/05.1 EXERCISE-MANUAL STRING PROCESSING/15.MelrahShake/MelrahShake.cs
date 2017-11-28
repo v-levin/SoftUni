1e083c676355bc4b3bb3450446045cc8b91e7492
Viktor Levin <viktortus@yahoo.com>
2017-11-28 16:40:07 +0100

    task added

---
+++ 3.1.1 C# Advanced/05.1 EXERCISE-MANUAL STRING PROCESSING/15.MelrahShake/MelrahShake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _15.MelrahShake
+{
+    public class MelrahShake
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var pattern = Console.ReadLine();
+
+            while (text.Contains(pattern) && !string.IsNullOrEmpty(pattern))
+            {
+                var firstIndexOfPattern = text.IndexOf(pattern);
+                var lastIndexOfPattern = text.LastIndexOf(pattern);
+
+                // check if found at least TWO matches
+                if (firstIndexOfPattern == lastIndexOfPattern)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Shaked it.");
+
+                // remove first and last occurance of pattern
+                text = text.Remove(lastIndexOfPattern, pattern.Length).Remove(firstIndexOfPattern, pattern.Length);
+                
+                var indexToRemove = pattern.Length / 2;
+                pattern = pattern.Remove(indexToRemove, 1);
+            }
+
+            Console.WriteLine("No shake.");
+            Console.WriteLine(text);
+        }
+    }
+}
