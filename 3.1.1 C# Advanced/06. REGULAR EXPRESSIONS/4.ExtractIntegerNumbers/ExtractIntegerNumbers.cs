5385c99bd4308cc30629c426c254c670d42203a7
Viktor Levin <viktortus@yahoo.com>
2017-11-29 20:06:08 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/4.ExtractIntegerNumbers/ExtractIntegerNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _4.ExtractIntegerNumbers
+{
+    public class ExtractIntegerNumbers
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var pattern = @"\d+";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text);
+
+            foreach (Match number in matches)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
