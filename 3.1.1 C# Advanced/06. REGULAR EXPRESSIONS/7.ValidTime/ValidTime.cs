64d804976252447322bf02d8ed10027de6a0641d
Viktor Levin <viktortus@yahoo.com>
2017-11-29 23:49:44 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/7.ValidTime/ValidTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _7.ValidTime
+{
+    public class ValidTime
+    {
+        public static void Main()
+        {
+            var time = Console.ReadLine();
+
+            var pattern = @"^(0[0-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] [AP]M$";
+            var regex = new Regex(pattern);
+
+            while (time != "END")
+            {
+                var match = regex.Match(time);
+
+                if (match.Success)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+
+                time = Console.ReadLine();
+            }
+        }
+    }
+}
