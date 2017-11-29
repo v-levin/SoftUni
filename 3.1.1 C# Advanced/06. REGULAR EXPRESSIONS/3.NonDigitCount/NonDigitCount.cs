9e15e6eaa5763e0ee89595928bc3df237a3cdf21
Viktor Levin <viktortus@yahoo.com>
2017-11-29 14:28:17 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/3.NonDigitCount/NonDigitCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3.NonDigitCount
+{
+    public class NonDigitCount
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var pattern = @"\D";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text);
+
+            Console.WriteLine($"Non-digits: {matches.Count}");
+        }
+    }
+}
