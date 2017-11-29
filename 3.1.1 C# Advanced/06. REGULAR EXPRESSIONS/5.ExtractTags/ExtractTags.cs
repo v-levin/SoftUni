e1c02b1004f37aa01f685419a36dcce064528fd2
Viktor Levin <viktortus@yahoo.com>
2017-11-29 20:39:46 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/5.ExtractTags/ExtractTags.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _5.ExtractTags
+{
+    public class ExtractTags
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"<.+?>";
+
+            while (input != "END")
+            {
+                var regex = new Regex(pattern);
+                var matches = regex.Matches(input);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
