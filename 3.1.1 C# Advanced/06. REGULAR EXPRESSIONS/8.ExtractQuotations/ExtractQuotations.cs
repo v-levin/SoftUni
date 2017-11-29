676e4e709b5454ba8e1cfdcecdffdbf2c6b424e1
Viktor Levin <viktortus@yahoo.com>
2017-11-30 00:11:55 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/8.ExtractQuotations/ExtractQuotations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _8.ExtractQuotations
+{
+    public class ExtractQuotations
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var pattern = @"('|"")(.+?)\1";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[2].Value);
+            }
+        }
+    }
+}
