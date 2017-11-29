9baee9c4bc31191e1110e2a6b1a73b005e870ce6
Viktor Levin <viktortus@yahoo.com>
2017-11-29 14:20:01 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/2.VowelCount/VowelCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2.VowelCount
+{
+    public class VowelCount
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var pattern = @"[aeiouyAEIOUY]";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text);
+
+            Console.WriteLine($"Vowels: {matches.Count}");
+        }
+    }
+}
