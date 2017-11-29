9ce311e41e1cf9f3611241f7f9c617f6dc501f03
Viktor Levin <viktortus@yahoo.com>
2017-11-29 21:17:19 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/6.ValidUsernames/ValidUsernames.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _6.ValidUsernames
+{
+    public class ValidUsernames
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"^[\w-]{3,16}$";
+            var regex = new Regex(pattern);
+
+            while (input != "END")
+            {
+                var isMatch = regex.IsMatch(input);
+
+                if (isMatch)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
