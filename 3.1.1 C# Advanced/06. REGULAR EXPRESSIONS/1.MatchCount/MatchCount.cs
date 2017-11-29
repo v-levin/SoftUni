fe60611679c1044a53532eed8ff21db4d4a0addc
Viktor Levin <viktortus@yahoo.com>
2017-11-29 14:10:08 +0100

    task added

---
+++ 3.1.1 C# Advanced/06. REGULAR EXPRESSIONS/1.MatchCount/MatchCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1.MatchCount
+{
+    public class MatchCount
+    {
+        public static void Main()
+        {
+            var pattern = Console.ReadLine();
+            var input = Console.ReadLine();
+
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+
+            Console.WriteLine(matches.Count);
+        }
+    }
+}
