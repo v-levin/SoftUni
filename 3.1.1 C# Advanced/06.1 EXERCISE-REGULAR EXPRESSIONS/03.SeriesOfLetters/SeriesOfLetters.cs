6762ed0512525dee1962fbd84f20062ea4842368
Viktor Levin <viktortus@yahoo.com>
2017-11-30 19:53:12 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/03.SeriesOfLetters/SeriesOfLetters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.SeriesOfLetters
+{
+    public class SeriesOfLetters
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"(\w)\1+";
+            var regex = new Regex(pattern);
+            var match = regex.Match(input);
+
+            var result = regex.Replace(input, "$1");
+
+            Console.WriteLine(result);
+        }
+    }
+}
