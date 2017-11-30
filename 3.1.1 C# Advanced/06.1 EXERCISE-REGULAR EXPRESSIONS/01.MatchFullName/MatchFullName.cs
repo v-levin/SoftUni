bc1a511f38650d59814374eb87a1942234592cf3
Viktor Levin <viktortus@yahoo.com>
2017-11-30 12:39:01 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/01.MatchFullName/MatchFullName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    public class MatchFullName
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"[A-Z][a-z]+\s[A-Z][a-z]+$";
+            var regex = new Regex(pattern);
+
+            while (input != "end")
+            {
+                var match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    Console.WriteLine(match);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
