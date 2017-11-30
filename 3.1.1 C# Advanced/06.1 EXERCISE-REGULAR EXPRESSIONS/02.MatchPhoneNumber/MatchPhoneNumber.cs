36d88b36b2d815e5ce88b7b396ebc0a7227fd24d
Viktor Levin <viktortus@yahoo.com>
2017-11-30 17:03:16 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/02.MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    public class MatchPhoneNumber
+    {
+        public static void Main()
+        {
+            var phoneNumber = Console.ReadLine();
+
+            var pattern = @"^\+359( |-)2\1\d{3}\1\d{4}$";
+            var regex = new Regex(pattern);
+
+            while (phoneNumber != "end")
+            {
+                var match = regex.Match(phoneNumber);
+
+                if (match.Success)
+                {
+                    Console.WriteLine(match);
+                }
+
+                phoneNumber = Console.ReadLine();
+            }
+        }
+    }
+}
