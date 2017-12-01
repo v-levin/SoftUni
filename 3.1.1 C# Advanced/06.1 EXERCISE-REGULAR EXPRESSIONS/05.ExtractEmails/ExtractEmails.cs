531d321eaa40fca76433e3d0c614e09f2528f2a1
Viktor Levin <viktortus@yahoo.com>
2017-12-01 17:32:51 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/05.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05.ExtractEmails
+{
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+
+            var pattern = @"(?<= )([a-zA-Z]+)[-_\.]?\w+?@[a-zA-Z]+[-]?[a-zA-Z]+\.[a-zA-Z]+\.?[a-zA-Z]+";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
