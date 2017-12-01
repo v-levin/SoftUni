b73fdc5d1ffeb6ae7178d3797503f042f44394e0
Viktor Levin <viktortus@yahoo.com>
2017-12-01 22:44:39 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/08.ExtractHyperlinks/ExtractHyperlinks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08.ExtractHyperlinks
+{
+    public class ExtractHyperlinks
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var text = new StringBuilder();
+
+            while (inputLine != "END")
+            {
+                text.Append(inputLine + " ");
+
+                inputLine = Console.ReadLine();
+            }
+
+            var pattern = @"<a.*?(?<!"">)href\s*=\s*(['""])?(.*?)(?:>|class|\1)";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text.ToString());
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[2].Value.Trim());
+            }
+        }
+    }
+}
