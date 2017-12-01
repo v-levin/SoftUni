783f8c693bd5cbc3c07f89664426b862edf94d5d
Viktor Levin <viktortus@yahoo.com>
2017-12-01 18:18:32 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/06.SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.SentenceExtractor
+{
+    public class SentenceExtractor
+    {
+        public static void Main()
+        {
+            var keyword = Console.ReadLine();
+            var text = Console.ReadLine();
+
+            var pattern = $@".+?\s{keyword}\s.+?[!\.?]";
+            var regex = new Regex(pattern);
+            var matches = isSentence(text);
+
+            foreach (Match match in matches)
+            {
+                var rgx = new Regex($@"\b{keyword}\b");
+                var containsKeyword = rgx.IsMatch(match.Value);
+
+                if (containsKeyword)
+                {
+                    Console.WriteLine(match.Value.Trim());
+                }
+            }
+        }
+
+        private static MatchCollection isSentence(string text)
+        {
+            var pattern = @"[^!?.]+[!?.]";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(text);
+
+            return matches;
+        }
+    }
+}
