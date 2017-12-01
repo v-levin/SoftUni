02fd1604350979d8176ecb7aa7cb0b74f245ad55
Viktor Levin <viktortus@yahoo.com>
2017-12-01 14:24:57 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/04.ReplaceATag/ReplaceATag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _04.ReplaceATag
+{
+    public class ReplaceATag
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"<a\.*(.+?)>(.+?)<\/a>";
+
+            while (input != "end")
+            {
+                input = Regex.Replace(input, pattern, @"[URL$1]$2[/URL]");
+                Console.WriteLine(input);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
