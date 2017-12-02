d2c4a5ad4f5154891a5bba1f3736fead35619060
Viktor Levin <viktortus@yahoo.com>
2017-12-02 17:43:26 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/09.QueryMess/QueryMess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _09.QueryMess
+{
+    public class QueryMess
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var pattern = @"([^&=?]*)=([^&=]*)";
+            var regex = new Regex(pattern);
+            
+
+            while (input != "END")
+            {
+                var queries = new Dictionary<string, List<string>>();
+                var matches = regex.Matches(input);
+
+                foreach (Match match in matches)
+                {
+                    // replace '+' and '%20' with 'space' 
+                    var whiteSpacePattern = @"(?:%20|\+)+";
+                    var key = match.Groups[1].Value;
+                    key = Regex.Replace(key, whiteSpacePattern, " ").Trim();
+                    var value = match.Groups[2].Value;
+                    value = Regex.Replace(value, whiteSpacePattern, " ").Trim();
+
+                    if (!queries.ContainsKey(key))
+                    {
+                        queries.Add(key, new List<string>());
+                    }
+
+                    queries[key].Add(value);
+                }
+
+                foreach (var query in queries)
+                {
+                    Console.Write($"{query.Key}=[{string.Join(", ", query.Value)}]");
+                }
+
+                Console.WriteLine();
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
