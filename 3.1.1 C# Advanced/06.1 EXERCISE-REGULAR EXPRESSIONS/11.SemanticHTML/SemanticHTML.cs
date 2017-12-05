99d92eddbc7230edfa9ba5a66f836ebde8efaf52
Viktor Levin <viktortus@yahoo.com>
2017-12-05 13:45:33 +0100

    task added

---
+++ 3.1.1 C# Advanced/06.1 EXERCISE-REGULAR EXPRESSIONS/11.SemanticHTML/SemanticHTML.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _11.SemanticHTML
+{
+    public class SemanticHTML
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+
+            var openDivPattern = @"<(div\s+)(?:.+?|)((?:\s*id|\s*class)\s*=\s*['""](main|header|nav|article|section|aside|footer)['""])";
+            var closeDivPattenr = @"</div>\s*<!--\s*(\w+)\s*-->";
+
+            var openDivRegex = new Regex(openDivPattern);
+            var closeDivRegex = new Regex(closeDivPattenr);
+
+            var result = new StringBuilder();
+
+            while (inputLine != "END")
+            {
+                var openDivMatch = openDivRegex.Match(inputLine);
+                var closeDivMatch = closeDivRegex.Match(inputLine);
+
+                if (openDivMatch.Success)
+                {
+                    inputLine = Regex.Replace(inputLine, openDivMatch.Groups[1].Value, openDivMatch.Groups[3].Value + " ");
+                    inputLine = Regex.Replace(inputLine, openDivMatch.Groups[2].Value, string.Empty);
+                    inputLine = Regex.Replace(inputLine, @"\s+", " ");
+                    inputLine = Regex.Replace(inputLine, @"\s+>", ">");
+                }
+
+                if (closeDivMatch.Success)
+                {
+                    inputLine = Regex.Replace(inputLine, closeDivMatch.Groups[0].Value, $"</{closeDivMatch.Groups[1].Value}>");
+                }
+
+                result.Append($"{inputLine}{Environment.NewLine}");
+
+                inputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
