5fe0dec0936055c4a63c61b919d61faf4b654eff
Viktor Levin <viktortus@yahoo.com>
2017-12-06 21:50:04 +0100

    task added

---
+++ 3.1.1 C# Advanced/07. FUNCTIONAL PROGRAMMING/4.AddVAT/AddVAT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.AddVAT
+{
+    public class AddVAT
+    {
+        public static void Main()
+        {
+            Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n * 1.2)
+                .ToList()
+                .ForEach(n => Console.WriteLine($"{n:n2}"));
+        }
+    }
+}
