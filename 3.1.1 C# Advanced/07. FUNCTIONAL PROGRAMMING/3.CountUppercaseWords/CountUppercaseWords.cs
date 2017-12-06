2114eba470285e6bd64d00bcf2896424b3427354
Viktor Levin <viktortus@yahoo.com>
2017-12-06 20:28:08 +0100

    task added

---
+++ 3.1.1 C# Advanced/07. FUNCTIONAL PROGRAMMING/3.CountUppercaseWords/CountUppercaseWords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.CountUppercaseWords
+{
+    public class CountUppercaseWords
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> checker = w => w[0] == w.ToUpper()[0];
+
+            words.Where(checker).ToList().ForEach(w => Console.WriteLine(w));
+        }
+    }
+}
