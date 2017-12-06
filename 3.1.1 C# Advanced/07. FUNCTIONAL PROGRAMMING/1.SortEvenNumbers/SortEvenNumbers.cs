36aaf454c80431209293b446de9a09d3e0227723
Viktor Levin <viktortus@yahoo.com>
2017-12-06 16:44:23 +0100

    task added

---
+++ 3.1.1 C# Advanced/07. FUNCTIONAL PROGRAMMING/1.SortEvenNumbers/SortEvenNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SortEvenNumbers
+{
+    public class SortEvenNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine();
+
+            var evenNumbers = numbers
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
