419f9cc4196f0b18cd74d8d642c8f8a16544854e
Viktor Levin <viktortus@yahoo.com>
2017-12-08 20:42:43 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/03.CustomMinFunction/CustomMinFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03.CustomMinFunction
+{
+    public class CustomMinFunction
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split();
+            Func<string[], int> minFunc = GetMinNumber;
+
+            var minNumber = minFunc(numbers);
+            Console.WriteLine(minNumber);
+        }
+
+        private static int GetMinNumber(string[] numbers)
+        {
+            var min = int.MaxValue;
+
+            foreach (var number in numbers)
+            {
+                if (int.Parse(number) < min)
+                {
+                    min = int.Parse(number);
+                }
+            }
+
+            return min;
+        }
+    }
+}
