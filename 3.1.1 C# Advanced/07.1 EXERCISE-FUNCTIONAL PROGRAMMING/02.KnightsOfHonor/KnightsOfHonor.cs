b5ede73e469afbe077080cc2b7747aae5ea20fc5
Viktor Levin <viktortus@yahoo.com>
2017-12-08 16:48:53 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/02.KnightsOfHonor/KnightsOfHonor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.KnightsOfHonor
+{
+    public class KnightsOfHonor
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split();
+            Action<string> printWithPrefix = n => Console.WriteLine($"Sir {n}");
+            PrintAllNames(names, printWithPrefix);
+        }
+
+        private static void PrintAllNames(string[] names, Action<string> print)
+        {
+            foreach (var name in names)
+            {
+                print(name);
+            }
+        }
+    }
+}
