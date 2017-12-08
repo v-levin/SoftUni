7f3bc99d11103c0cc928010528c1f65f4af93b1e
Viktor Levin <viktortus@yahoo.com>
2017-12-08 16:45:36 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    public class ActionPrint
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split();
+            Action<string> print = n => Console.WriteLine(n);
+            PrintAllNames(names, print);
+        }
+
+        private static void PrintAllNames(string[] names, Action<string> print)
+        {
+            foreach (var name in names)
+            {
+                print(name);
+            }
+        }
+    }
+}
