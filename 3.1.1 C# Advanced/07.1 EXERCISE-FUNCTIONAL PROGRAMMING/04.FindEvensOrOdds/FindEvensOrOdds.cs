f6fa0ec359e850edd7faebc4e12d280864f39875
Viktor Levin <viktortus@yahoo.com>
2017-12-08 21:07:00 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/04.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    public class FindEvensOrOdds
+    {
+        public static void Main()
+        {
+            var rangeBounds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var lowerBound = rangeBounds[0];
+            var upperBound = rangeBounds[1];
+            var command = Console.ReadLine();
+
+            var numbers = Enumerable.Range(lowerBound, upperBound - lowerBound + 1);
+            Predicate<int> isEven = n => n % 2 == 0;
+
+            PrintNumbers(numbers, command, isEven);
+        }
+
+        private static void PrintNumbers(IEnumerable<int> numbers, string command, Predicate<int> isEven)
+        {
+            var result = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (isEven(number) && command == "even")
+                {
+                    result.Add(number);
+                }
+                else if (!isEven(number) && command == "odd")
+                {
+                    result.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
