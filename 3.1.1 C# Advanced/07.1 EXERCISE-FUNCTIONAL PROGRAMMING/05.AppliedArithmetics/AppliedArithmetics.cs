cc6384544c5f80f20d9d28519ddb503f67ba62af
Viktor Levin <viktortus@yahoo.com>
2017-12-09 14:03:19 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/05.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    public class AppliedArithmetics
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                Func<int, int> applyArithmetics = GetArithmetics(command);
+                numbers = numbers.Select(applyArithmetics).ToArray();
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static Func<int, int> GetArithmetics(string command)
+        {
+            switch (command)
+            {
+                case "add": return n => n + 1;
+                case "multiply": return n => n * 2;
+                case "subtract": return n => n - 1;
+                default: return n => n;
+            }
+        }
+    }
+}
