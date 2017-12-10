dac6d0058070525e6d9832c408af6f7e1e77dc64
Viktor Levin <viktortus@yahoo.com>
2017-12-10 15:38:13 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07.PredicateForNames
+{
+    public class PredicateForNames
+    {
+        public static void Main()
+        {
+            var nameLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+
+            Func<string, bool> isValidName = n => n.Length <= nameLength;
+
+            PrintResult(names, isValidName);
+        }
+
+        private static void PrintResult(string[] names, Func<string, bool> isValidName)
+        {
+            foreach (var name in names)
+            {
+                if (isValidName(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
