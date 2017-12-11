456f916ceb6b4d45d9c15062bb317f930cf0f689
Viktor Levin <viktortus@yahoo.com>
2017-12-11 18:00:38 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/11.PartyReservationFilterModule/PartyReservationFilterModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.PartyReservationFilterModule
+{
+    public class PartyReservationFilterModule
+    {
+        public static void Main()
+        {
+            var people = Console.ReadLine().Split().ToList();
+            var commands = Console.ReadLine().Split(';');
+
+            var filters = new List<string>();
+
+            while (commands[0] != "Print")
+            {
+                switch (commands[0])
+                {
+                    case "Add filter":
+                        filters.Add($"{commands[1]} {commands[2]}");
+                        break;
+                    case "Remove filter":
+                        filters.Remove($"{commands[1]} {commands[2]}");
+                        break;
+                    default:
+                        break;
+                }
+
+                commands = Console.ReadLine().Split(';');
+            }
+
+            foreach (var filter in filters)
+            {
+                var filterCommands = filter.Split();
+
+                switch (filterCommands[0])
+                {
+                    case "Starts":
+                        people = people.Where(p => !p.StartsWith(filterCommands[2])).ToList();
+                        break;
+                    case "Ends":
+                        people = people.Where(p => !p.EndsWith(filterCommands[2])).ToList();
+                        break;
+                    case "Length":
+                        people = people.Where(p => p.Length != int.Parse(filterCommands[1])).ToList();
+                        break;
+                    case "Contains":
+                        people = people.Where(p => !p.Contains(filterCommands[1])).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", people));
+        }
+    }
+}
