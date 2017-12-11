199c3628dd35b383ae3b940b0a715cb387636d71
Viktor Levin <viktortus@yahoo.com>
2017-12-11 23:08:19 +0100

    task added

---
+++ 3.1.1 C# Advanced/07.1 EXERCISE-FUNCTIONAL PROGRAMMING/13.TriFunction/TriFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _13.TriFunction
+{
+    public class TriFunction
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray().Select(c => (int)c).Sum() >= num;
+            Func<string[], int, Func<string, int, bool>, string> firstValidName = (namesArr, num, func) => namesArr.FirstOrDefault(str => func(str, num));
+
+            var result = firstValidName(names, n, isValidWord);
+            Console.WriteLine(result);
+        }
+    }
+}
