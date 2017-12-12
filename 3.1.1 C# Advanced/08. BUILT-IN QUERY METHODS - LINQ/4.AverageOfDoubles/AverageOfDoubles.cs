4fc7734a717102e46680fad95eb3c430203c464f
Viktor Levin <viktortus@yahoo.com>
2017-12-12 17:48:41 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/4.AverageOfDoubles/AverageOfDoubles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.AverageOfDoubles
+{
+    public class AverageOfDoubles
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            var average = numbers.Average();
+
+            Console.WriteLine($"{average:F2}");
+        }
+    }
+}
