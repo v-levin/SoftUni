6a62bad9a097c5bc1bd7a20c6d1d99208588fe35
Viktor Levin <viktortus@yahoo.com>
2017-12-12 19:51:43 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/7.BoundedNumbers/BoundedNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _7.BoundedNumbers
+{
+    public class BoundedNumbers
+    {
+        public static void Main()
+        {
+            var boundaries = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var minBound = boundaries.Min();
+            var maxBound = boundaries.Max();
+
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Where(n => n >= minBound && n <= maxBound)
+                .ToList();
+
+            numbers.ForEach(n => Console.Write(n + " "));
+        }
+    }
+}
