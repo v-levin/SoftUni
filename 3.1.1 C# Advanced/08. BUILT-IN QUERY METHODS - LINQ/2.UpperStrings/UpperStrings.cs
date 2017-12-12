247c8736c05094fe123a641ef5ffa151923701be
Viktor Levin <viktortus@yahoo.com>
2017-12-12 14:14:44 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/2.UpperStrings/UpperStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.UpperStrings
+{
+    public class UpperStrings
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            words.Select(n => n.ToUpper())
+                .ToList()
+                .ForEach(w => Console.Write(w + " "));
+        }
+    }
+}
