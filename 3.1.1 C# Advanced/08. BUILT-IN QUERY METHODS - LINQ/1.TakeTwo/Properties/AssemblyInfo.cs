fbeb18910322264c4a07a2553f110e9503ce8545
Viktor Levin <viktortus@yahoo.com>
2017-12-12 14:10:21 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/1.TakeTwo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("1.TakeTwo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1.TakeTwo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fd882d2-1349-421b-b4ed-c1b1d2fdcd20")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/1.TakeTwo/TakeTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.TakeTwo
+{
+    public class TakeTwo
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            numbers
+                .Where(n => n >= 10 && n <= 20)
+                .Distinct()
+                .Take(2)
+                .ToList()
+                .ForEach(n => Console.Write(n + " "));
+
+        }
+    }
+}
