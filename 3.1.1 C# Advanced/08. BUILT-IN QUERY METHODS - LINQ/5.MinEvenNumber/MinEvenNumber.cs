8f897ec7ec760e8c6c1baea59d544f86ff90ac31
Viktor Levin <viktortus@yahoo.com>
2017-12-12 17:58:14 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/5.MinEvenNumber/MinEvenNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.MinEvenNumber
+{
+    public class MinEvenNumber
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .Where(n => n % 2 == 0)
+                .ToList();
+
+            if (numbers.Any())
+            {
+                var minNumber = numbers.Min();
+
+                Console.WriteLine($"{minNumber:F2}");
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}
