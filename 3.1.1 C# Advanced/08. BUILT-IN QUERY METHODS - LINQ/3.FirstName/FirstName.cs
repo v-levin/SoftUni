9277e3cc2d62994a62b9bd04fa2fc89c01aa162b
Viktor Levin <viktortus@yahoo.com>
2017-12-12 14:50:06 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/3.FirstName/FirstName.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.FirstName
+{
+    public class FirstName
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine()
+                .Split()
+                .OrderBy(n => n)
+                .ToList();
+
+            var letters = Console.ReadLine()
+                .Split()
+                .Select(l => l.ToLower())
+                .OrderBy(l => l)
+                .ToList();
+
+            var result = new List<string>();
+
+            foreach (var letter in letters)
+            {
+                result.AddRange(names.Where(n => n.ToLower().StartsWith(letter)));
+            }
+
+            if (result.Any())
+            {
+                Console.WriteLine(result.First());
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}
