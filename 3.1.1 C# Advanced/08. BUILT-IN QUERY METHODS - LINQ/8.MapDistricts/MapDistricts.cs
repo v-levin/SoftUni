2c16225bdb9b259c0b113530b4011c5346160043
Viktor Levin <viktortus@yahoo.com>
2017-12-12 20:31:21 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/8.MapDistricts/MapDistricts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8.MapDistricts
+{
+    public class MapDistricts
+    {
+        public static void Main()
+        {
+            var population = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var minPopulation = long.Parse(Console.ReadLine());
+
+            var districtsMap = new Dictionary<string, List<long>>();
+
+            foreach (var district in population)
+            {
+                var tokens = district.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                var city = tokens[0];
+                var districtPopulation = long.Parse(tokens[1]);
+
+                if (!districtsMap.ContainsKey(city))
+                {
+                    districtsMap.Add(city, new List<long>());
+                }
+
+                districtsMap[city].Add(districtPopulation);
+            }
+
+            districtsMap = districtsMap
+                .Where(p => p.Value.Sum() >= minPopulation)
+                .OrderByDescending(p => p.Value.Sum())
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var district in districtsMap)
+            {
+                Console.WriteLine($"{district.Key}: {string.Join(" ", district.Value.OrderByDescending(p => p).Take(5))}");
+            }
+            
+        }
+    }
+}
