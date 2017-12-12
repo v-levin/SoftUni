2731e7c6051c2669e6c8a8285b89f42c524a3a37
Viktor Levin <viktortus@yahoo.com>
2017-12-12 18:31:34 +0100

    task added

---
+++ 3.1.1 C# Advanced/08. BUILT-IN QUERY METHODS - LINQ/6.FindAndSumIntegers/FindAndSumIntegers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _6.FindAndSumIntegers
+{
+    public class FindAndSumIntegers
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Where(s => !s.All(char.IsLetter))
+                .Select(long.Parse)
+                .ToList();
+
+            if (input.Any())
+            {
+                var sum = input.Sum();
+
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("No match");
+            }
+        }
+    }
+}
