b49cd1b60ecc031de1823ce8fd66e0084dcf8b0e
Viktor Levin <viktortus@yahoo.com>
2017-12-13 17:50:52 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/03.StudentsByAge/StudentsByAge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.StudentsByAge
+{
+    public class StudentsByAge
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var students = new Dictionary<string, int>();
+
+            while (input != "END")
+            {
+                var lastIndexOfSpace = input.LastIndexOf(' ');
+                var studentFullName = input.Substring(0, lastIndexOfSpace);
+                var age = int.Parse(input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Last());
+
+                students.Add(studentFullName, age);
+
+                input = Console.ReadLine();
+            }
+
+            var result = students.Where(s => s.Value >= 18 && s.Value <= 24)
+                .Select(s => $"{s.Key} {s.Value}")
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
