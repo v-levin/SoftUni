1684b36d89793fac3bbcc5be897a35cf5faf91af
Viktor Levin <viktortus@yahoo.com>
2017-12-13 20:37:21 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/05.FilterStudentsByEmailDomain/FilterStudentsByEmailDomain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FilterStudentsByEmailDomain
+{
+    public class FilterStudentsByEmailDomain
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var studentEmails = new Dictionary<string, string>();
+
+            while (input != "END")
+            {
+                var lastSpaceIndex = input.LastIndexOf(' ');
+                var fullName = input.Substring(0, lastSpaceIndex);
+                var email = input.Split().Last();
+
+                studentEmails.Add(fullName, email);
+
+                input = Console.ReadLine();
+            }
+
+            var result = studentEmails
+                .Where(s => s.Value.Contains("gmail"))
+                .Select(s => s.Key)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
