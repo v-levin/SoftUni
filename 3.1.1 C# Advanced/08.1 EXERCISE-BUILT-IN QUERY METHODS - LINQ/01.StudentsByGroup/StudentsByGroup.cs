5ee417063d0cbf36ac35f2b945ea9f333c19828a
Viktor Levin <viktortus@yahoo.com>
2017-12-13 12:52:43 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/01.StudentsByGroup/StudentsByGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.StudentsByGroup
+{
+    public class StudentsByGroup
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Trim();
+
+            var students = new Dictionary<string, int>();
+
+            while (input != "END")
+            {
+                var lastIndexOfSpace = input.LastIndexOf(' ');
+                var studentFullName = input.Substring(0, lastIndexOfSpace);
+                var groupNumber = int.Parse(input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Last());
+
+                students.Add(studentFullName, groupNumber);
+
+                input = Console.ReadLine().Trim();
+            }
+
+            var result = students.Where(s => s.Value == 2)
+                .Select(s => s.Key)
+                .OrderBy(s => s)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
