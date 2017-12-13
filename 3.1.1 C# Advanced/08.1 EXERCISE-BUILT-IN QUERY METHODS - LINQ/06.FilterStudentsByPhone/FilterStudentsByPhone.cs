cc053f0bca3a552e8551ad15a9a450670de8280e
Viktor Levin <viktortus@yahoo.com>
2017-12-13 23:30:07 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/06.FilterStudentsByPhone/FilterStudentsByPhone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.FilterStudentsByPhone
+{
+    public class FilterStudentsByPhone
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var studentsPhone = new Dictionary<string, string>();
+
+            while (input != "END")
+            {
+                var indexOfLastSpace = input.LastIndexOf(' ');
+                var fullName = input.Substring(0, indexOfLastSpace);
+                var phoneNumber = input.Split().Last();
+
+                studentsPhone.Add(fullName, phoneNumber);
+
+                input = Console.ReadLine();
+            }
+
+            var result = studentsPhone
+                .Where(s => s.Value.StartsWith("02") || s.Value.StartsWith("+3592"))
+                .Select(s => s.Key)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
