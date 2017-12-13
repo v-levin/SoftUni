b1b7ba90bfcc5776034a115fede84d77ad2b5037
Viktor Levin <viktortus@yahoo.com>
2017-12-13 20:23:27 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/04.SortStudents/SortStudents.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SortStudents
+{
+    public class SortStudents
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var students = new List<KeyValuePair<string, string>>();
+
+            while (input != "END")
+            {
+                var tokens = input.Split();
+                var firstName = tokens[0];
+                var lastName = tokens[1];
+
+                students.Add(new KeyValuePair<string, string>(firstName, lastName));
+
+                input = Console.ReadLine();
+            }
+
+            var result = students.OrderBy(s => s.Value)
+                .ThenByDescending(s => s.Key)
+                .Select(s => $"{s.Key} {s.Value}")
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
