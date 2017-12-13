369aed16f93f6d163f52639bc8931a34f413e89b
Viktor Levin <viktortus@yahoo.com>
2017-12-13 15:31:42 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/02.StudentsByFirstAndLastName/StudentsByFirstAndLastName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StudentsByFirstAndLastName
+{
+    public class StudentsByFirstAndLastName
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var students = new List<KeyValuePair<string, string>>();
+
+            while (input != "END")
+            {
+                var tokens = input.Split();
+                var firstName = tokens[0];
+                var lastName = tokens[1];
+
+                students.Add(new KeyValuePair<string, string>(firstName, lastName));
+
+                input = Console.ReadLine();
+            }
+
+            var result = students.Where(s => s.Key.CompareTo(s.Value) == -1)
+                .ToList();
+
+            foreach (var student in result)
+            {
+                Console.WriteLine($"{student.Key} {student.Value}");
+            }
+        }
+    }
+}
