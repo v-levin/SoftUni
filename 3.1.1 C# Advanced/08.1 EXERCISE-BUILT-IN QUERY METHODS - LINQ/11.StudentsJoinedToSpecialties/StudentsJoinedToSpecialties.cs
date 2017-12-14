6fb48861ba93f4dbf330e5b44e26c536ca27545a
Viktor Levin <viktortus@yahoo.com>
2017-12-14 18:11:25 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/11.StudentsJoinedToSpecialties/StudentsJoinedToSpecialties.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.StudentsJoinedToSpecialties
+{
+    public class StudentSpecialty
+    {
+        public string specialtyName;
+
+        public int facultyNumber;
+
+        public StudentSpecialty(string specialtyName, int facultyNumber)
+        {
+            this.specialtyName = specialtyName;
+            this.facultyNumber = facultyNumber;
+        }
+    }
+
+    public class Student
+    {
+        public string studentName;
+
+        public int facultyNumber;
+
+        public Student(string studentName, int facultyNumber)
+        {
+            this.studentName = studentName;
+            this.facultyNumber = facultyNumber;
+        }
+    }
+
+    public class StudentsJoinedToSpecialties
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var specialties = new List<StudentSpecialty>();
+            var students = new List<Student>();
+
+            while (input != "Students:")
+            {
+                var inputArgs = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var specialtyName = inputArgs[0] + " " + inputArgs[1];
+                var facultyNumber = int.Parse(inputArgs[2]);
+
+                var studentSpecialty = new StudentSpecialty(specialtyName, facultyNumber);
+                specialties.Add(studentSpecialty);
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var inputArgs = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var studentName = inputArgs[1] + " " + inputArgs[2];
+                var facultyNumber = int.Parse(inputArgs[0]);
+
+                var student = new Student(studentName, facultyNumber);
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            var result = from sp in specialties
+                         join st in students on sp.facultyNumber equals st.facultyNumber
+                         orderby st.studentName
+                         select new { st.studentName, st.facultyNumber, sp.specialtyName };
+
+            foreach (var student in result)
+            {
+                Console.WriteLine($"{student.studentName} {student.facultyNumber} {student.specialtyName}");
+            }
+        }
+    }
+}
