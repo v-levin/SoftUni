f9c8f366e17e3359d577cc082150b01bc319cc9b
Viktor Levin <viktortus@yahoo.com>
2017-12-14 13:39:48 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/09.StudentsEnrolledIn2014Or2015/StudentsEnrolledIn2014Or2015.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.StudentsEnrolledIn2014Or2015
+{
+    public class StudentsEnrolledIn2014Or2015
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var students = new List<int[]>();
+
+            while (input != "END")
+            {
+                var inputArgs = input.Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                students.Add(inputArgs);
+
+                input = Console.ReadLine();
+            }
+
+            students
+                .Where(s => s[0] % 100 == 14 || s[0] % 100 == 15)
+                .Select(s => s.Skip(1))
+                .ToList()
+                .ForEach(s => Console.WriteLine(string.Join(" ", s)));
+        }
+    }
+}
