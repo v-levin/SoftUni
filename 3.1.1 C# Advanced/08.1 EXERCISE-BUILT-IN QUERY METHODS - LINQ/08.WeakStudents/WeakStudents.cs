21527a986c0f995c33ec91ae65857a00f1cb8e59
Viktor Levin <viktortus@yahoo.com>
2017-12-14 13:27:41 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/08.WeakStudents/WeakStudents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.WeakStudents
+{
+    public class WeakStudents
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var students = new List<string[]>();
+
+            while (input != "END")
+            {
+                var inputArgs = input.Split();
+
+                students.Add(inputArgs);
+
+                input = Console.ReadLine();
+            }
+
+            students
+                .Where(s => s.Skip(2).Count(n => int.Parse(n) <= 3) >= 2)
+                .Select(s => $"{s[0]} {s[1]}")
+                .ToList()
+                .ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
