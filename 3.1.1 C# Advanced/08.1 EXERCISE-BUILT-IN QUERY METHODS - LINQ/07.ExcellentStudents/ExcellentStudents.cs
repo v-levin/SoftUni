4935ea39cda1256dcf94b8074c5de704b5c56651
Viktor Levin <viktortus@yahoo.com>
2017-12-14 13:06:16 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/07.ExcellentStudents/ExcellentStudents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.ExcellentStudents
+{
+    public class ExcellentStudents
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var students = new List<string>();
+
+            while (input != "END")
+            {
+                students.Add(input);
+
+                input = Console.ReadLine();
+            }
+
+            var excellentStudents = students
+                .Where(s => s.Contains("6"))
+                .Select(s => s.Split())
+                .Select(s => $"{s[0]} {s[1]}")
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, excellentStudents));
+        }
+    }
+}
