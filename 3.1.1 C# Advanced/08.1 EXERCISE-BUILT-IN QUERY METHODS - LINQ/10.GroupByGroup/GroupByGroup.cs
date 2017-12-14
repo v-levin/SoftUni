29964b0f22b71f69717bf1282ef4ce586bb6ae21
Viktor Levin <viktortus@yahoo.com>
2017-12-14 14:57:03 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/10.GroupByGroup/GroupByGroup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10.GroupByGroup
+{
+    public class Person
+    {
+        public string name;
+
+        public int group;
+
+        public Person(string name, int group)
+        {
+            this.name = name;
+            this.group = group;
+        }
+    }
+
+    public class GroupByGroup
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var students = new List<Person>();
+
+            while (input != "END")
+            {
+                var lastSpaceIndex = input.LastIndexOf(' ');
+                var fullName = input.Substring(0, lastSpaceIndex);
+                var group = int.Parse(input.Split().Last());
+
+                var student = new Person(fullName, group);
+
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            var sorted = students
+                .GroupBy(s => s.group)
+                .OrderBy(s => s.Key);
+
+            foreach (var outerElement in sorted)
+            {
+                Console.Write($"{outerElement.Key} - ");
+
+                var sb = new StringBuilder();
+                foreach (var student in outerElement)
+                {
+                    sb.Append($"{student.name}, ");
+                }
+
+                sb.Remove(sb.Length - 2, 1);
+                Console.WriteLine(sb);
+            }
+        }
+    }
+}
