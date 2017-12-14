753a4e8a81955f4cdd4bc8deb43e8e025fd23e8f
Viktor Levin <viktortus@yahoo.com>
2017-12-14 20:36:44 +0100

    task added

---
+++ 3.1.1 C# Advanced/08.1 EXERCISE-BUILT-IN QUERY METHODS - LINQ/12.LittleJohn/LittleJohn.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _12.LittleJohn
+{
+    public class LittleJohn
+    {
+        public static void Main()
+        {
+            var smallArrow = ">----->";
+            var mediumArrow = ">>----->";
+            var largeArrow = ">>>----->>";
+
+            var pattern = $@"(?:{smallArrow}|{mediumArrow}|{largeArrow})";
+            var arrowsMatch = new Regex(pattern);
+
+            var smallArrowsCount = 0;
+            var mediumArrowsCount = 0;
+            var largeArrowsCount = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                var input = Console.ReadLine();
+                var isMatch = arrowsMatch.IsMatch(input);
+
+                if (isMatch)
+                {
+                    var matches = arrowsMatch.Matches(input);
+
+                    foreach (Match match in matches)
+                    {
+                        if (match.Value == smallArrow)
+                        {
+                            smallArrowsCount++;
+                        }
+                        else if (match.Value == mediumArrow)
+                        {
+                            mediumArrowsCount++;
+                        }
+                        else if (match.Value == largeArrow)
+                        {
+                            largeArrowsCount++;
+                        }
+                    }
+                }
+            }
+
+
+            var numAsString = $"{smallArrowsCount}{mediumArrowsCount}{largeArrowsCount}";
+
+            var decNumber = int.Parse(numAsString);
+
+            var binNumber = Convert.ToString(decNumber, 2);
+            var reversedBin = new string(binNumber.Reverse().ToArray());
+            var totalBin = binNumber + reversedBin;
+
+            var encryptedCountOfArrows = Convert.ToInt32(totalBin, 2);
+
+            Console.WriteLine(encryptedCountOfArrows);
+        }
+    }
+}
