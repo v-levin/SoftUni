83c8096a11cd39d56ffe1df7745d1d6dc767404a
Viktor Levin <viktortus@yahoo.com>
2017-12-15 18:39:08 +0100

    bashsoft task added

---
+++ 3.1.1 C# Advanced/08.2 EXERCISE-FUNCTIONAL PROGRAMMING AND LINQ/BashSoft/BashSoft/Repository/RepositorySorters.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class RepositorySorters
+    {
+        public static void OrderAndTake(Dictionary<string, List<int>> wantedData, string comparison, int studentsToTake)
+        {
+
+        }
+
+        private static void OrderAndTake(Dictionary<string, List<int>> wantedData, int studentsToTake, 
+            Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
+        {
+
+        }
+
+        private static int CompareInOrder(KeyValuePair<string, List<int>> firstValue, KeyValuePair<string, List<int>> secondValue)
+        {
+            int totalOdFirstMarks = 0;
+            foreach (var i in firstValue.Value)
+            {
+                totalOdFirstMarks += i;
+            }
+
+            int totalOfSecondMarks = 0;
+            foreach (var i in secondValue.Value)
+            {
+                totalOfSecondMarks += i;
+            }
+
+            return totalOfSecondMarks.CompareTo(totalOdFirstMarks);
+        }
+
+        private static int CompareDescendingOrder(KeyValuePair<string, List<int>> firstValue, KeyValuePair<string, List<int>> secondValue)
+        {
+            int totalOdFirstMarks = 0;
+            foreach (var i in firstValue.Value)
+            {
+                totalOdFirstMarks += i;
+            }
+
+            int totalOfSecondMarks = 0;
+            foreach (var i in secondValue.Value)
+            {
+                totalOfSecondMarks += i;
+            }
+
+            return totalOdFirstMarks.CompareTo(totalOfSecondMarks);
+        }
+    }
+}
