be0901debf01448d04e4b2a002b946907cbc25d9
Viktor Levin <viktortus@yahoo.com>
2017-12-17 13:49:56 +0100

    bashsoft task added

--- 3.1.1 C# Advanced/08.2 EXERCISE-FUNCTIONAL PROGRAMMING AND LINQ/BashSoft/BashSoft/Repository/RepositoryFilters.cs
+++ 3.1.1 C# Advanced/08.2 EXERCISE-FUNCTIONAL PROGRAMMING AND LINQ/BashSoft/BashSoft/Repository/RepositoryFilters.cs
@@ -12,15 +12,15 @@ namespace BashSoft
         {
             if (wantedFilter == "excellent")
             {
-                FilterAndTake(wantedData, ExcellentFilter, studentsToTake);
+                FilterAndTake(wantedData, x => x >= 5, studentsToTake);
             }
             else if (wantedFilter == "average")
             {
-                FilterAndTake(wantedData, AverageFilter, studentsToTake);
+                FilterAndTake(wantedData, x => x < 5 && x >= 3.5, studentsToTake);
             }
             else if (wantedFilter == "poor")
             {
-                FilterAndTake(wantedData, PoorFilter, studentsToTake);
+                FilterAndTake(wantedData, x => x < 3.5, studentsToTake);
             }
             else
             {
@@ -38,42 +38,16 @@ namespace BashSoft
                     break;
                 }
 
-                double averageMark = Average(userName_Points.Value);
-                if (givenFilter(averageMark))
+                double averageScore = userName_Points.Value.Average();
+                double percentageOfFullfilment = averageScore / 100;
+                double mark = percentageOfFullfilment * 4 + 2;
+
+                if (givenFilter(mark))
                 {
                     OutputWriter.PrintStudent(userName_Points);
                     counterForPrinted++;
                 }
             }
-        }
-
-        private static bool ExcellentFilter(double mark)
-        {
-            return mark >= 5.0;
-        }
-
-        private static bool AverageFilter(double mark)
-        {
-            return mark < 5.0 && mark >= 3.5;
-        }
-
-        private static bool PoorFilter(double mark)
-        {
-            return mark < 3.5;
-        }
-
-        private static double Average(List<int> scoresOnTasks)
-        {
-            int totalScore = 0;
-            foreach (var score in scoresOnTasks)
-            {
-                totalScore += score;
-            }
-
-            var percentageOfAll = totalScore / (scoresOnTasks.Count * 100);
-            var mark = percentageOfAll * 4 + 2;
-
-            return mark;
         }
     }
 }
