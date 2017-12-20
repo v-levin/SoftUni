49d6ba3b1c25c3283dc01fd4f48c65a38d889805
Viktor Levin <viktortus@yahoo.com>
2017-12-20 13:26:51 +0100

    task added

---
+++ 3.1.1 C# Advanced/EXAM PREPARATION II/1.SecondNature/SecondNature.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.SecondNature
+{
+    public class SecondNature
+    {
+        public static void Main()
+        {
+            var flowers = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Reverse());
+
+            var buckets = new Stack<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse));
+
+            var secondNature = new Queue<int>();
+            var currentFlower = 0;
+
+            while (flowers.Count > 0 && buckets.Count > 0)
+            {
+                currentFlower = flowers.Pop();
+
+                while (buckets.Count > 0 && currentFlower - buckets.Peek() > 0)
+                    currentFlower -= buckets.Pop();
+
+                var reminder = 0;
+                if (buckets.Count > 0)
+                {
+                    if (currentFlower - buckets.Peek() < 0)
+                    {
+                        reminder = buckets.Pop() - currentFlower;
+                    }
+                    else
+                    {
+                        secondNature.Enqueue(currentFlower);
+                        buckets.Pop();
+                    }
+
+                    currentFlower = -1;
+
+                    if (buckets.Count > 0)
+                        buckets.Push(buckets.Pop() + reminder);
+                    else if (reminder > 0)
+                        buckets.Push(reminder);
+                }
+
+                if (currentFlower > 0)
+                {
+                    flowers.Push(currentFlower);
+                }
+            }
+
+            if (flowers.Count > 0)
+                Console.WriteLine(string.Join(" ", flowers));
+
+            if (buckets.Count > 0)
+                Console.WriteLine(string.Join(" ", buckets));
+
+            if (secondNature.Count > 0)
+                Console.WriteLine(string.Join(" ", secondNature));
+            else
+                Console.WriteLine("None");
+        }
+    }
+}
