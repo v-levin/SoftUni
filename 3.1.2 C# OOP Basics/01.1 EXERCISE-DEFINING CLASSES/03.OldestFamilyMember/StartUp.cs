05beffa741ca59f67ace63e24dd274cd473684da
Viktor Levin <viktortus@yahoo.com>
2017-12-21 14:47:26 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace OldestFamilyMember
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfPeople = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var personInfo = Console.ReadLine().Split();
+                var personName = personInfo[0];
+                var personAge = int.Parse(personInfo[1]);
+
+                var person = new Person(personName, personAge);
+                family.AddMember(person);
+            }
+
+            var oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
