b9bbbc451e77495857d72aecb05e55c151211419
Viktor Levin <viktortus@yahoo.com>
2017-12-21 23:25:37 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public double CalculateDifference(string date1, string date2)
+        {
+            var d1 = date1.Split().Select(int.Parse).ToArray();
+            var d2 = date2.Split().Select(int.Parse).ToArray();
+
+            var firstDate = new DateTime(d1[0], d1[1], d1[2]);
+            var secondDate = new DateTime(d2[0], d2[1], d2[2]);
+
+            return Math.Abs((firstDate - secondDate).TotalDays);
+        }
+    }
+}
---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/05.DateModifier/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var dateModifier = new DateModifier();
+
+            var result = dateModifier.CalculateDifference(firstDate, secondDate);
+
+            Console.WriteLine(result);
+        }
+    }
+}
