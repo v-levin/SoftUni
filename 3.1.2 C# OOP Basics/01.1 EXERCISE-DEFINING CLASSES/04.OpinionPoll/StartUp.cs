cbbf1d0d0138a918eddc516edc037bf74be79f66
Viktor Levin <viktortus@yahoo.com>
2017-12-21 23:00:01 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OpinionPoll
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfPeople = int.Parse(Console.ReadLine());
+            var people = new List<Person>();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var personInfo = Console.ReadLine().Split();
+                var personName = personInfo[0];
+                var personAge = int.Parse(personInfo[1]);
+
+                var person = new Person(personName, personAge);
+
+                people.Add(person);
+            }
+
+            people
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToList()
+                .ForEach(p => Console.WriteLine($"{p.Name} - {p.Age}"));
+        }
+    }
+}
