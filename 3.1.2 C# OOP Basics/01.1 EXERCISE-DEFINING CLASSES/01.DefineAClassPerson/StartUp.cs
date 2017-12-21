b2d80d0b4fb25831f4837497f731c4e41ae284a1
Viktor Levin <viktortus@yahoo.com>
2017-12-21 13:25:59 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineAClassPerson
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            var person = new Person()
+            {
+                name = "Pesho",
+                age = 20
+            };
+
+            var secondPerson = new Person();
+            secondPerson.name = "Gosho";
+            secondPerson.age = 18;
+
+            var thirdPerson = new Person();
+            thirdPerson.name = "Stamat";
+            thirdPerson.age = 43;
+        }
+    }
+}
