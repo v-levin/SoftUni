fb69a454d45ac86d987543ed68b9bc64326cc039
Viktor Levin <viktortus@yahoo.com>
2017-12-22 13:36:16 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/06.CompanyRoster/Employee.cs
@@ -0,0 +1,47 @@
+﻿namespace CompanyRoster
+{
+    public class Employee
+    {
+        private string name;
+        private decimal salary;
+        private string position;
+        private string department;
+        private string email;
+        private int age;
+
+        public Employee(string name, decimal salary, string position, string department)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.position = position;
+            this.department = department;
+            this.email = "n/a";
+            this.age = -1;
+        }
+
+        public decimal Salary
+        {
+            get { return this.salary; }
+        }
+
+        public string Email
+        {
+            set { this.email = value; }
+        }
+
+        public int Age
+        {
+            set { this.age = value; }
+        }
+
+        public string Department
+        {
+            get { return this.department; }
+        }
+
+        public string PrintEmployeeInfo()
+        {
+            return $"{this.name} {this.salary:F2} {this.email} {this.age}";
+        }
+    }
+}
---
+++ 3.1.2 C# OOP Basics/01.1 EXERCISE-DEFINING CLASSES/06.CompanyRoster/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyRoster
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfEmployees = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                var employeeInfo = Console.ReadLine().Split();
+                var name = employeeInfo[0];
+                var salary = decimal.Parse(employeeInfo[1]);
+                var position = employeeInfo[2];
+                var department = employeeInfo[3];
+
+                var employee = new Employee(name, salary, position, department);
+
+                if (employeeInfo.Length > 4)
+                {
+                    if (int.TryParse(employeeInfo[4], out int age))
+                    {
+                        employee.Age = age;
+                    }
+                    else
+                    {
+                        var email = employeeInfo[4];
+                        employee.Email = email;
+                    }
+                }
+
+                if (employeeInfo.Length > 5)
+                {
+                    var age = int.Parse(employeeInfo[5]);
+                    employee.Age = age;
+                }
+
+                employees.Add(employee);
+            }
+
+            var highestAverageSalaryDepartment = employees
+                .GroupBy(em => em.Department)
+                .Select(gr => new
+                {
+                    Name = gr.Key,
+                    AverageSalary = gr.Average(em => em.Salary),
+                    Employees = gr
+                })
+                .OrderByDescending(gr => gr.AverageSalary)
+                .FirstOrDefault();
+
+            Console.WriteLine($"Highest Average Salary: {highestAverageSalaryDepartment.Name}");
+
+            foreach (var emp in highestAverageSalaryDepartment.Employees.OrderByDescending(em => em.Salary))
+            {
+                Console.WriteLine(emp.PrintEmployeeInfo());
+            }
+        }
+    }
+}
