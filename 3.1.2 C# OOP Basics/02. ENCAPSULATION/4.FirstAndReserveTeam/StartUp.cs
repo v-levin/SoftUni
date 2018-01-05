358de20cba4b4b595258cf54446c38844fb4b0e6
v-levin <viktortus@yahoo.com>
2018-01-05 19:32:06 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/02. ENCAPSULATION/4.FirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        var personCount = int.Parse(Console.ReadLine());
+
+        var team = new Team("Exodus");
+
+        for (int i = 0; i < personCount; i++)
+        {
+            var personInfo = Console.ReadLine().Split();
+            var person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), double.Parse(personInfo[3]));
+
+            team.AddPlayer(person);
+        }
+
+        Console.WriteLine($"First team have {team.FirstTeam.Count} players");
+        Console.WriteLine($"Reserve team have {team.ReserveTeam.Count} players");
+    }
+}
