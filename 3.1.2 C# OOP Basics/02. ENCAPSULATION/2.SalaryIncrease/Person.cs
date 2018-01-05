4c09d502fba68801e01b5666ad37b647c2762bb0
v-levin <viktortus@yahoo.com>
2018-01-05 16:41:28 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/02. ENCAPSULATION/2.SalaryIncrease/Person.cs
@@ -0,0 +1,42 @@
+﻿public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+    }
+
+    public void IncreaseSalary(double percent)
+    {
+        if (this.Age > 30)
+        {
+            this.salary += this.salary * percent / 100;
+        }
+        else
+        {
+            this.salary += this.salary * percent / 200;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} get {this.salary:f2} leva";
+    }
+}
