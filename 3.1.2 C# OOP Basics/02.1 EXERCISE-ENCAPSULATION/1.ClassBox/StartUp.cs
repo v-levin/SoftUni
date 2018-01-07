5752c06b7254d6a1a1377b68dfbb17d1271fc17c
v-levin <viktortus@yahoo.com>
2018-01-07 23:11:33 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/02.1 EXERCISE-ENCAPSULATION/1.ClassBox/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ClassBox
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+
+            var boxParams = new double[3];
+            for (int i = 0; i < boxParams.Length; i++)
+            {
+                boxParams[i] = double.Parse(Console.ReadLine());
+            }
+
+            var box = new Box(boxParams[0], boxParams[1], boxParams[2]);
+
+            Console.WriteLine($"Surface Area - {box.GetSurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.GetVolume():f2}");
+        }
+    }
+}
