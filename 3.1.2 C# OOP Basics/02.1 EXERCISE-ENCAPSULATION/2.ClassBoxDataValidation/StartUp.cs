d1d6335e3a06a1ca2599c5d58324025617bd97ad
v-levin <viktortus@yahoo.com>
2018-01-08 11:39:34 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/02.1 EXERCISE-ENCAPSULATION/2.ClassBoxDataValidation/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ClassBoxDataValidation
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Type boxType = typeof(Box);
+            FieldInfo[] fields = boxType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine(fields.Count());
+
+            try
+            {
+                var boxParams = new double[3];
+                for (int i = 0; i < boxParams.Length; i++)
+                {
+                    boxParams[i] = double.Parse(Console.ReadLine());
+                }
+
+                var box = new Box(boxParams[0], boxParams[1], boxParams[2]);
+
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.GetVolume():f2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
