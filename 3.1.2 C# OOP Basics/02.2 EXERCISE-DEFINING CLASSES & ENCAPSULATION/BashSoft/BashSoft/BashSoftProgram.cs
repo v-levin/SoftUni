d44a97067193dc51694fefddafaa39ba33333048
v-levin <viktortus@yahoo.com>
2018-01-16 12:21:12 +0100

    BashSoft project OOP tasks added

---
+++ 3.1.2 C# OOP Basics/02.2 EXERCISE-DEFINING CLASSES & ENCAPSULATION/BashSoft/BashSoft/BashSoftProgram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public class BashSoftProgram
+    {
+        public static void Main()
+        {
+            // Lab 1 Tests
+            //IOManager.TraverseDirectory(@"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft");
+            //IOManager.TraverseDirectory();
+            //IOManager.TraverseDirectory(0);
+
+            // Lab 2 Tests
+            //StudentsRepository.InitializeData();
+            //StudentsRepository.GetAllStudentsFromCourse("Unity");
+            //StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
+
+            // Lab 3 Tests
+            //Tester.CompareContent(@"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft-FirstWeek-Tasks\03. CSharp-Advanced-Files-And-Directories-Lab\test2.txt", @"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft-FirstWeek-Tasks\03. CSharp-Advanced-Files-And-Directories-Lab\test3.txt");
+            //Tester.CompareContent(@"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft-FirstWeek-Tasks\04. CSharp-Advanced-Exception-Handling-Lab\expected.txt", @"D:\SoftUni\3.1.1 C# Advanced\02.2 EXERCISE-LINEAR STRUCTURES - OVERVIEW\BashSoft-FirstWeek-Tasks\04. CSharp-Advanced-Exception-Handling-Lab\actual.txt");
+            //IOManager.CreateDirectoryInCurrentFolder("pesho");
+            //IOManager.TraverseDirectory(900);
+
+            // Lab 4 Tests
+            //IOManager.ChangeCurrentDirectoryAbsolute(@"C:\Windows");
+            //IOManager.TraverseDirectory(20);
+            //IOManager.CreateDirectoryInCurrentFolder("*2");
+
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+            //IOManager.ChangeCurrentDirectoryRelative("..");
+
+            // Lab 5 Test
+            //InputReader.StartReadingCommands();
+
+            Tester tester = new Tester();
+            IOManager ioManager = new IOManager();
+            StudentRepository repo = new StudentRepository(new RepositoryFilter(), new RepositorySorter());
+
+            CommandInterpreter currentInterpreter = new CommandInterpreter(tester, repo, ioManager);
+            InputReader reader = new InputReader(currentInterpreter);
+
+            reader.StartReadingCommands();
+        }
+    }
+}
---
+++ 3.1.2 C# OOP Basics/02.2 EXERCISE-DEFINING CLASSES & ENCAPSULATION/BashSoft/BashSoft/Repository/RepositoryFilter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public class RepositoryFilter
+    {
+        public void FilterAndTake(Dictionary<string, double> studentsWithMarks, string wantedFilter, int studentsToTake)
+        {
+            if (wantedFilter == "excellent")
+            {
+                FilterAndTake(studentsWithMarks, x => x >= 5, studentsToTake);
+            }
+            else if (wantedFilter == "average")
+            {
+                FilterAndTake(studentsWithMarks, x => x < 5 && x >= 3.5, studentsToTake);
+            }
+            else if (wantedFilter == "poor")
+            {
+                FilterAndTake(studentsWithMarks, x => x < 3.5, studentsToTake);
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidStudentFilter);
+            }
+        }
+
+        private void FilterAndTake(Dictionary<string, double> StudentWithMarks, Predicate<double> givenFilter, int studentsToTake)
+        {
+            int counterForPrinted = 0;
+            foreach (var studentMark in StudentWithMarks)
+            {
+                if (counterForPrinted == studentsToTake)
+                {
+                    break;
+                }
+
+                if (givenFilter(studentMark.Value))
+                {
+                    OutputWriter.PrintStudent(new KeyValuePair<string, double>(studentMark.Key, studentMark.Value));
+                    counterForPrinted++;
+                }
+            }
+        }
+    }
+}
