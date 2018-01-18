e5cb5e4b9a47a9ab28b74665aadc1416eaaf5ecb
v-levin <viktortus@yahoo.com>
2018-01-18 17:06:41 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/03.1 EXERCISE-INHERITANCE/2.BookShop/Book.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace BookShop
+{
+    public class Book
+    {
+        private string author;
+        private string title;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public string Author
+        {
+            get { return this.author; }
+            set
+            {
+                var authorParams = value.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (authorParams.Length > 1)
+                {
+                    var secondName = authorParams[1];
+                    if (char.IsDigit(secondName[0]))
+                    {
+                        throw new ArgumentException("Author not valid!");
+                    }
+                }
+
+                this.author = value;
+            }
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                this.title = value;
+            }
+        }
+
+        public virtual decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Type: ").AppendLine(this.GetType().Name)
+                .Append("Title: ").AppendLine(this.Title)
+                .Append("Author: ").AppendLine(this.Author)
+                .Append("Price: ").Append($"{this.Price:f1}")
+                .AppendLine();
+
+            return sb.ToString();
+        }
+    }
+}
