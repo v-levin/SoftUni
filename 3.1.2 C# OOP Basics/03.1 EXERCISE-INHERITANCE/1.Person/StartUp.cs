2f0817e99fa8fff443f99aeefc58d7b0e83245a1
v-levin <viktortus@yahoo.com>
2018-01-18 15:28:58 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/03.1 EXERCISE-INHERITANCE/1.Person/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
