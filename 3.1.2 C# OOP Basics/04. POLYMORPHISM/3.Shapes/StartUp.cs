77a3b9889946cc4284a089fa190977833b372228
v-levin <viktortus@yahoo.com>
2018-01-23 16:34:24 +0100

    task added

---
+++ 3.1.2 C# OOP Basics/04. POLYMORPHISM/3.Shapes/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Shape rectangle = new Rectangle(3, 4.3);
+        Shape circle = new Circle(6.4);
+
+        Console.WriteLine(rectangle.CalculateArea());
+        Console.WriteLine(rectangle.CalculatePerimeter());
+        Console.WriteLine(rectangle.Draw());
+
+        Console.WriteLine(circle.CalculateArea());
+        Console.WriteLine(circle.CalculatePerimeter());
+        Console.WriteLine(circle.Draw());
+    }
+}
