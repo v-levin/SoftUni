f934799e2532d402d1dea8408d3ce9ca4704ace3
v-levin <viktortus@yahoo.com>
2018-02-16 18:54:28 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/01. INTERFACES AND ABSTRACTION/2.Cars/Tesla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Tesla : ICar, IElectricCar
+{
+    public Tesla(string model, string color, int battery)
+    {
+        this.Model = model;
+        this.Color = color;
+        this.Battery = battery;
+    }
+
+    public string Model { get; }
+
+    public string Color { get; }
+
+    public int Battery { get; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Baterries" +
+            $"{Environment.NewLine}" +
+            $"{this.Start()}" +
+            $"{Environment.NewLine}" +
+            $"{this.Stop()}";
+    }
+}
