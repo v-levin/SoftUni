b002170b93573861058e3246dbb4a141da01182d
v-levin <viktortus@yahoo.com>
2018-03-14 19:44:23 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/01.1 EXERCISE-INTERFACES AND ABSTRACTION/10.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using ExplicitInterfaces.Interfaces;
+using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "End")
+            {
+                var tokens = inputLine.Split();
+                var name = tokens[0];
+                var country = tokens[1];
+                var age = int.Parse(tokens[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+                IPerson person = citizen;
+                IResident resident = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
