70cb26a0c33aab21f412280bdf498203cb686f8a
v-levin <viktortus@yahoo.com>
2018-03-15 12:09:13 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/02. GENERICS/1.BoxOfT/Box.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class Box<T>
+{
+    private Stack<T> data;
+
+    public Box()
+    {
+        this.data = new Stack<T>();
+    }
+
+    public int Count => this.data.Count;
+
+    public void Add(T element)
+    {
+        this.data.Push(element);
+    }
+
+    public T Remove()
+    {
+        return this.data.Pop();
+    }
+}
