b774632586ad465b926925c60ecd2a195e64ce39
v-levin <viktortus@yahoo.com>
2018-03-16 14:10:39 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/02.1 EXERCISE-GENERICS/03.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Box<int> boxInt = new Box<int>(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine(boxInt);
+            }
+        }
+    }
+}
