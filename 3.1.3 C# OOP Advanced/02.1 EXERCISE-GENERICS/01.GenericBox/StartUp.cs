6845484e5098d4e9d895e2191e86c61e960ab6b1
v-levin <viktortus@yahoo.com>
2018-03-16 13:54:09 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/02.1 EXERCISE-GENERICS/01.GenericBox/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericBox
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Box<int> boxInt = new Box<int>(int.Parse(Console.ReadLine()));
+            Box<string> boxStr = new Box<string>(Console.ReadLine());
+
+            Console.WriteLine(boxInt);
+            Console.WriteLine(boxStr);
+        }
+    }
+}
