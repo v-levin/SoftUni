f1b1addb7efe3ee46044ab83f6f666dea03f658c
v-levin <viktortus@yahoo.com>
2018-03-16 16:52:19 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/02.1 EXERCISE-GENERICS/04.GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            List<Box<string>> listOfBoxes = new List<Box<string>>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Box<string> boxStr = new Box<string>(Console.ReadLine());
+                listOfBoxes.Add(boxStr);
+            }
+
+            var indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstIndex = indexes[0];
+            var secondIndex = indexes[1];
+
+            SwapElements(listOfBoxes, firstIndex, secondIndex);
+
+            foreach (var box in listOfBoxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+        private static void SwapElements<T>(List<Box<T>> listOfBoxes, int firstIndex, int secondIndex)
+        {
+            Box<T> tempBox = listOfBoxes[firstIndex];
+            listOfBoxes[firstIndex] = listOfBoxes[secondIndex];
+            listOfBoxes[secondIndex] = tempBox;
+        }
+    }
+}
