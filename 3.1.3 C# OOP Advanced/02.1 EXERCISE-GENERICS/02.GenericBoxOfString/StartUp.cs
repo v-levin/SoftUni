ad45cd57b776af20cbe3033f6af897ca1cda43b1
v-levin <viktortus@yahoo.com>
2018-03-16 14:05:35 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/02.1 EXERCISE-GENERICS/02.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Box<string> boxStr = new Box<string>(Console.ReadLine());
+
+                Console.WriteLine(boxStr);
+            }
+        }
+    }
+}
