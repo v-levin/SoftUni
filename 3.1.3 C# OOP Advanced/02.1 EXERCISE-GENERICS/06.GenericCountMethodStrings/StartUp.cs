b25ea8e5f88a4f8794f4d198284f3c0c49e165ea
v-levin <viktortus@yahoo.com>
2018-03-20 21:04:04 +0100

    task added

---
+++ 3.1.3 C# OOP Advanced/02.1 EXERCISE-GENERICS/06.GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            List<Box<string>> listOfBoxes = new List<Box<string>>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Box<string> boxStr = new Box<string>(Console.ReadLine());
+                listOfBoxes.Add(boxStr);
+            }
+
+            var element = Console.ReadLine();
+            var result = GetGreaterElementsCount(listOfBoxes, element);
+
+            Console.WriteLine(result);
+        }
+
+        public static int GetGreaterElementsCount<T>(List<Box<T>> listOfBoxes, T element)
+            where T : IComparable
+            => listOfBoxes.Count(x => x.Value.CompareTo(element) > 0);
+    }
+}
