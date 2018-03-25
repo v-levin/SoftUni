9f86b1a3f9499d9d87d8a04920e3d5471a79df67
v-levin <viktortus@yahoo.com>
2018-03-25 13:56:51 +0200

    task added

---
+++ 3.1.3 C# OOP Advanced/02.1 EXERCISE-GENERICS/11.Tuple/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputTokens = Console.ReadLine().Split();
+            var names = $"{inputTokens[0]} {inputTokens[1]}";
+            var address = inputTokens[2];
+            var namesAddress = new Tuple<string, string>(names, address);
+            Console.WriteLine(namesAddress);
+
+            inputTokens = Console.ReadLine().Split();
+            var name = inputTokens[0];
+            var amountOfBeer = int.Parse(inputTokens[1]);
+            var nameAmountOfBeer = new Tuple<string, int>(name, amountOfBeer);
+            Console.WriteLine(nameAmountOfBeer);
+
+            inputTokens = Console.ReadLine().Split();
+            var intParam = int.Parse(inputTokens[0]);
+            var doubleParam = double.Parse(inputTokens[1]);
+            var intDouble = new Tuple<int, double>(intParam, doubleParam);
+            Console.WriteLine(intDouble);
+        }
+    }
+}
