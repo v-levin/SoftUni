e8e34b5374b9e881254acad0630c0369cf56521b
v-levin <viktortus@yahoo.com>
2018-04-01 18:55:04 +0200

    task added

---
+++ 3.1.3 C# OOP Advanced/03.1 EXERCISE-ITERATORS AND COMPARATORS/2.Collection/ListyIterator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private IList<T> elements;
+        private int currentIndex;
+
+        public ListyIterator(IEnumerable<T> elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex < this.elements.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.elements[currentIndex]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in this.elements)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/03.1 EXERCISE-ITERATORS AND COMPARATORS/2.Collection/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Collection
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ListyIterator<string> iterator = null;
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var inputArgs = inputLine.Split();
+
+                switch (inputArgs[0])
+                {
+                    case "Create":
+                        iterator = new ListyIterator<string>(inputArgs.Skip(1));
+                        break;
+                    case "Move":
+                        Console.WriteLine(iterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(iterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            iterator.Print();
+                        }
+                        catch (ArgumentException ae)
+                        {
+                            Console.WriteLine(ae.Message);
+                        }
+                        break;
+                    case "PrintAll":
+                        foreach (var item in iterator)
+                        {
+                            Console.Write($"{item} ");
+                        }
+
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
