c98b8f0c4c3e9b61d872e4f080d6e95f3479ec53
v-levin <viktortus@yahoo.com>
2018-04-01 17:21:01 +0200

    task added

---
+++ 3.1.3 C# OOP Advanced/03.1 EXERCISE-ITERATORS AND COMPARATORS/1.ListyIterator/ListyIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private IList<T> elements;
+        private int currentIndex;
+
+        public ListyIterator(IEnumerable<T> elements)
+        {
+            this.elements = new List<T>(elements);
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex < this.elements.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.elements.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.elements[currentIndex]);
+        }
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/03.1 EXERCISE-ITERATORS AND COMPARATORS/1.ListyIterator/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ListyIterator<string> iterator = null;
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var inputArgs = inputLine.Split();
+
+                switch (inputArgs[0])
+                {
+                    case "Create":
+                        iterator = new ListyIterator<string>(inputArgs.Skip(1));
+                        break;
+                    case "Move":
+                        Console.WriteLine(iterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(iterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            iterator.Print();
+                        }
+                        catch (ArgumentException ae)
+                        {
+                            Console.WriteLine(ae.Message);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
