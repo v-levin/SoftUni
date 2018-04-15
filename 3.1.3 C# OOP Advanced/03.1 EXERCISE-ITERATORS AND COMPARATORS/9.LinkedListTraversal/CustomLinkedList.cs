7b03814972f1530a06d28cb3dd821c9807711e59
v-levin <viktortus@yahoo.com>
2018-04-15 23:17:59 +0200

    task added

---
+++ 3.1.3 C# OOP Advanced/03.1 EXERCISE-ITERATORS AND COMPARATORS/9.LinkedListTraversal/CustomLinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedListTraversal
+{
+    public class CustomLinkedList<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        public class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node Next { get; set; }            
+        }
+
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(T item)
+        {
+            Node old = this.Tail;
+            this.Tail = new Node(item);
+
+            if (IsEmpty())
+            {
+                this.Head = this.Tail;
+            }
+            else
+            {
+                old.Next = this.Tail;
+            }
+
+            this.Count++;
+        }
+
+        public void Remove(T item)
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+
+            if (this.Head.Value.CompareTo(item) == 0)
+            {
+                this.Head = this.Head.Next;
+                this.Count--;
+                return;
+            }
+
+            Node previous = this.Head;
+            Node current = this.Head.Next;
+
+            while (current != null)
+            {
+                if (current.Value.CompareTo(item) == 0)
+                {
+                    previous.Next = current.Next;
+                    this.Count--;
+                    return;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+        }
+
+        private bool IsEmpty()
+        {
+            return (this.Count == 0) ? true : false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = this.Head;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/03.1 EXERCISE-ITERATORS AND COMPARATORS/9.LinkedListTraversal/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LinkedListTraversal
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CustomLinkedList<int> list = new CustomLinkedList<int>();
+
+            var operationsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < operationsCount; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var command = tokens[0];
+                var number = int.Parse(tokens[1]);
+
+                switch (command)
+                {
+                    case "Add":
+                        list.Add(number);
+                        break;
+                    case "Remove":
+                        list.Remove(number);
+                        break;
+                }
+            }
+
+            Console.WriteLine(list.Count);
+
+            foreach (var item in list)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
