9eeb1d50509779b2aac44138edaa884ca8e10776
v-levin <viktortus@yahoo.com>
2018-04-23 22:14:40 +0200

    task added

--- 3.1.3 C# OOP Advanced/04. ENUMS AND ATTRIBUTES/3.CreateAttribute/SoftUniAttribute.cs
+++ 3.1.3 C# OOP Advanced/04. ENUMS AND ATTRIBUTES/3.CreateAttribute/SoftUniAttribute.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
-public class SoftUni : Attribute
+public class SoftUniAttribute : Attribute
 {
-    private string name;
-
-    public SoftUni(string name)
+    public SoftUniAttribute(string name)
     {
         this.Name = name;
     }
 
-    public string Name { get; private set; }
+    public string Name { get; set; }
 }
---
+++ 3.1.3 C# OOP Advanced/04. ENUMS AND ATTRIBUTES/4.CodingTracker/StartUp.cs
@@ -0,0 +1,10 @@
+﻿[SoftUni("Levin")]
+public class StartUp
+{
+    [SoftUni("Viktor")]
+    public static void Main()
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
