94435c33cd8f1a6c84bfb500794b5c7dfa267efa
v-levin <viktortus@yahoo.com>
2018-04-28 11:59:01 +0200

    task added

---
+++ 3.1.3 C# OOP Advanced/04.1 EXERCISE-ENUMS AND ATTRIBUTES/02.CardRank/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CardRank
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine($"{input}:");
+            
+            foreach (var value in Enum.GetValues(typeof(CardRank)))
+            {
+                Console.WriteLine($"Ordinal value: {(int)value}; Name value: {value}");
+            }
+        }
+    }
+}
