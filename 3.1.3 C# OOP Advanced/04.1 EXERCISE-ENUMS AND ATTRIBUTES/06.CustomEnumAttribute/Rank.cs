96a42fd04f711d33c90d70a1bbb7ad365023c34c
v-levin <viktortus@yahoo.com>
2018-05-01 12:39:10 +0200

    task added

---
+++ 3.1.3 C# OOP Advanced/04.1 EXERCISE-ENUMS AND ATTRIBUTES/06.CustomEnumAttribute/Rank.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomEnumAttribute
+{
+    [Type("Enumeration", "Rank", "Provides rank constants for a Card class.")]
+    public enum Rank
+    {
+        Ace = 14,
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/04.1 EXERCISE-ENUMS AND ATTRIBUTES/06.CustomEnumAttribute/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CustomEnumAttribute
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            if (input == "Rank")
+            {
+                PrintAttribute(typeof(Rank));
+            }
+            else
+            {
+                PrintAttribute(typeof(Suit));
+            }
+        }
+
+        private static void PrintAttribute(Type type)
+        {
+            var attributes = type.GetCustomAttributes(false);
+            Console.WriteLine(attributes[0]);
+        }
+    }
+}
