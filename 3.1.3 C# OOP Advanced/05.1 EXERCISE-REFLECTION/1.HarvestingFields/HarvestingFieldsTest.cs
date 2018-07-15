ba6a887eceb7a5d92dec6cdcd853b2d7dc33405d
v-levin <viktortus@yahoo.com>
2018-07-15 19:19:05 +0200

    add task

---
+++ 3.1.3 C# OOP Advanced/05.1 EXERCISE-REFLECTION/1.HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace HarvestingFields
+{
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type harvestingFieldsType = typeof(HarvestingFields);
+            FieldInfo[] harvestingFields = harvestingFieldsType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Dictionary<string, Func<FieldInfo[]>> accModFilters = new Dictionary<string, Func<FieldInfo[]>>()
+            {
+                { "public", () => harvestingFields.Where(f => f.IsPublic).ToArray() },
+                { "private", () => harvestingFields.Where(f => f.IsPrivate).ToArray() },
+                { "protected", () => harvestingFields.Where(f => f.IsFamily).ToArray() },
+                { "all", () => harvestingFields }
+            }; 
+
+            FieldInfo[] gatheredFields;
+            string requestedAccMod;
+            while ((requestedAccMod = Console.ReadLine()) != "HARVEST")
+            {
+                accModFilters[requestedAccMod]()
+                    .Select(f => $"{f.Attributes.ToString().ToLower()} {f.FieldType.Name} {f.Name}").ToArray()
+                    .ToList()
+                    .ForEach(r => Console.WriteLine(r.Replace("family", "protected")));
+            }
+        }
+    }
+}
