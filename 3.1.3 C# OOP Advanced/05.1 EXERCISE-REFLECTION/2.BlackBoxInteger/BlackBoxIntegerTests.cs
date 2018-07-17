4002aef7a1943dda4c0d318a15204456f7258051
v-levin <viktortus@yahoo.com>
2018-07-17 23:05:18 +0200

    add task

---
+++ 3.1.3 C# OOP Advanced/05.1 EXERCISE-REFLECTION/2.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace BlackBoxInteger
+{
+    public class BlackBoxIntegerTests
+    {
+        private const BindingFlags NonPublicFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+
+        public static void Main()
+        {
+            Type blackBoxType = typeof(BlackBoxInteger);
+            BlackBoxInteger myBlackBox = (BlackBoxInteger)Activator.CreateInstance(blackBoxType, true);
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var tokens = inputLine.Split('_');
+                var methodName = tokens[0];
+                var value = int.Parse(tokens[1]);
+
+                blackBoxType.GetMethod(methodName, NonPublicFlags)
+                    .Invoke(myBlackBox, new object[] { value });
+
+                var innerStateValue = blackBoxType
+                    .GetFields(NonPublicFlags)
+                    .First()
+                    .GetValue(myBlackBox);
+
+                Console.WriteLine(innerStateValue);
+            }
+        }
+    }
+}
