a5020ea8501c9543443081d7bdfcc3d84c65953b
v-levin <viktortus@yahoo.com>
2018-08-23 21:00:41 +0200

    add task

---
+++ 3.1.3 C# OOP Advanced/06. UNIT TESTING/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class DummyTests
+{
+    private const int AttackPoints = 10;
+    private const int DummyHealth = 25;
+    private const int DummyExperience = 10;
+
+    private Dummy dummy;
+
+    [SetUp]
+    public void InitDummy()
+    {
+        // Arrange
+        dummy = new Dummy(DummyHealth, DummyExperience);
+    }
+
+    [Test]
+    public void DummyLosesHealthIfAttacked()
+    {
+        // Act
+        dummy.TakeAttack(AttackPoints);
+        
+        // Assert
+        Assert.AreEqual(15, dummy.Health);
+    }
+
+    [Test]
+    public void DeadDummyThrowsExceptionIfAttacked()
+    {
+        // Act
+        while (!dummy.IsDead())
+            dummy.TakeAttack(AttackPoints);
+
+        // Assert
+        var ex = Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(AttackPoints));
+        Assert.That(ex.Message, Is.EqualTo("Dummy is dead."));
+    }
+
+    [Test]
+    public void DeadDummyCanGiveExperience()
+    {
+        // Act
+        while (!dummy.IsDead())
+            dummy.TakeAttack(AttackPoints);
+
+        var gotExperience = dummy.GiveExperience();
+
+        // Assert
+        Assert.AreEqual(DummyExperience, gotExperience);
+    }
+
+    [Test]
+    public void AliveDummyCannotGiveExperience()
+    {
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+    }
+}
