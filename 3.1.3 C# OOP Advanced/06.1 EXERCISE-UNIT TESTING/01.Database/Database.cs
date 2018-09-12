e77ece63bff6c547da54c889e8e08b2fd481eac9
v-levin <viktortus@yahoo.com>
2018-09-12 12:44:28 +0200

    add task

---
+++ 3.1.3 C# OOP Advanced/06.1 EXERCISE-UNIT TESTING/01.Database/Database.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace Database
+{
+    public class Database
+    {
+        private const int MaxCapacity = 16;
+        private const string CapacityExeptionMessage = "The max capacity of the collection is {MaxCapacity}.";
+        private const string EmptyCollectionExeptionMessage = "The collection is already empty.";
+
+        private readonly int[] elements;
+        private int currentIndex;
+
+        public Database(params int[] elements)
+        {
+            this.elements = new int[MaxCapacity];
+            this.currentIndex = 0;
+            this.Elements = elements.ToArray();
+        }
+
+        public int Index => this.currentIndex;
+
+        public int[] Elements
+        {
+            get => this.elements;
+            set
+            {
+                if (value.Length > MaxCapacity)
+                {
+                    throw new InvalidOperationException(CapacityExeptionMessage);
+                }
+
+                foreach (var element in value)
+                {
+                    this.elements[this.currentIndex] = element;
+                    this.currentIndex++;
+                }
+            }
+        }
+
+        public void Add(int element)
+        {
+            if (this.currentIndex >= MaxCapacity)
+            {
+                throw new InvalidOperationException(CapacityExeptionMessage);
+            }
+
+            this.elements[this.currentIndex] = element;
+            this.currentIndex++;
+        }
+
+        public void Remove()
+        {
+            if (this.currentIndex == 0)
+            {
+                throw new InvalidOperationException(EmptyCollectionExeptionMessage);
+            }
+
+            this.elements[this.currentIndex - 1] = 0;
+            this.currentIndex--;
+        }
+
+        public int[] Fetch()
+        {
+            return this.elements.Take(this.currentIndex).ToArray();
+        }
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/06.1 EXERCISE-UNIT TESTING/01.Database/DatabaseTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Database
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        private const int MaxCapacity = 16;
+        private const int ValidLength = 5;
+        private const int TestNumber = 12;
+
+        private Database database;
+
+        [SetUp]
+        public void InitDatabase()
+        {
+            this.database = new Database(new int[ValidLength]);
+        }
+
+        [Test]
+        public void ThrownExceptionIfMoreElelemtsThenTheCapasityArePassedThroughConstructor()
+        {
+            Assert.Throws<InvalidOperationException>(() => new Database(new int[MaxCapacity + 1]));
+        }
+
+        [Test]
+        public void AddElementShouldAddInTheNextFreeCell()
+        {
+            // Act
+            this.database.Add(TestNumber);
+            var firstIndex = database.Index;
+            this.database.Add(TestNumber);
+            var secondIndex = database.Index;
+
+            // Assert
+            Assert.AreEqual(firstIndex, secondIndex - 1);
+        }
+
+        [Test]
+        public void ExceptionThrownIfTryToInsertMoreElementsThanCapacity()
+        {
+            // Arrange
+            this.database = new Database(new int[MaxCapacity]);
+            
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => this.database.Add(TestNumber));
+        }
+
+        [Test]
+        public void RemoveElementShouldRemoveOnlyLastElement()
+        {
+            // Act
+            this.database.Add(TestNumber);
+            var lastIndex = this.database.Index;
+            this.database.Remove();
+            var currentIndex = this.database.Index;
+
+            // Assert
+            Assert.AreEqual(lastIndex, currentIndex + 1);
+        }
+
+        [Test]
+        public void ExceptionThrownWhenTryToRemoveElementFromEmptyDatabase()
+        {
+            // Arranage
+            this.database = new Database(new int[0]);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => this.database.Remove());
+        }
+
+        [Test]
+        public void FetchMethodShouldReturnElemetsAsArray()
+        {
+            // Assert
+            Assert.That(this.database.Fetch() is Array);
+        }
+    }
+}
