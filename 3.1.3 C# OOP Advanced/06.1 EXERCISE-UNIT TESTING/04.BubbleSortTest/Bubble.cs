6699c5ed7d8b8e66c199c9bafb86303223df2fba
Viktor Levin <viktortus@yahoo.com>
2018-09-21 14:08:02 +0200

    add task

---
+++ 3.1.3 C# OOP Advanced/06.1 EXERCISE-UNIT TESTING/04.BubbleSortTest/Bubble.cs
@@ -0,0 +1,26 @@
+﻿namespace BubbleSortTest
+{
+    public class Bubble
+    {
+        public void Sort(int[] numbers)
+        {
+            var swapped = true;
+
+            while (swapped)
+            {
+                swapped = false;
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        var temp = numbers[i];
+                        numbers[i] = numbers[i + 1];
+                        numbers[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/06.1 EXERCISE-UNIT TESTING/04.BubbleSortTest/Tests/BubbleTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace BubbleSortTest.Tests
+{
+    [TestFixture]
+    public class BubbleTests
+    {
+        [Test]
+        [TestCase(8, 2, 3, 4, 5, 6, 7, 9, 1)]
+        [TestCase(9, 8, 7, 6, 5, 4, 3, 2, 1)]
+        public void BubbleShouldSortCollectionCorrectly(params int[] numbers)
+        {
+            // Arrange
+            var bubble = new Bubble();
+            var sortedNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Act
+            bubble.Sort(numbers);
+
+            // Assert
+            CollectionAssert.AreEqual(sortedNumbers, numbers);
+        }
+    }
+}
