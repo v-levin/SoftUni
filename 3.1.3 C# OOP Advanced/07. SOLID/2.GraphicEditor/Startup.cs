f3c3f97a382632b9fb6a9ec049927bd162204f1f
Viktor Levin <viktortus@yahoo.com>
2018-09-24 13:32:30 +0200

    add task

---
+++ 3.1.3 C# OOP Advanced/07. SOLID/2.GraphicEditor/Startup.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphicEditor
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            IShape rectangle = new Rectangle();
+            IShape circle = new Circle();
+            IShape square = new Square();
+
+            var graphicEditor = new GraphicEditor();
+
+            graphicEditor.DrawShape(rectangle);
+            graphicEditor.DrawShape(circle);
+            graphicEditor.DrawShape(square);
+        }
+    }
+}
