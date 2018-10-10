e11714a5404ebe0cb60ebb5deb22e2cb7fbaed67
v-levin <viktortus@yahoo.com>
2018-10-10 16:04:24 +0200

    add skeleton

---
+++ 3.1.3 C# OOP Advanced/07.1 EXERCISE-SOLID/2.Blobs/Entities/Attacks/Attack.cs
@@ -0,0 +1,9 @@
+﻿using _Blobs.Interfaces;
+
+namespace _Blobs.Entities.Attacks
+{
+    public abstract class Attack : IAttack
+    {
+        public abstract void Execute(Blob attacker, Blob target);
+    }
+}
---
+++ 3.1.3 C# OOP Advanced/07.1 EXERCISE-SOLID/2.Blobs/Interfaces/IBehavior.cs
@@ -0,0 +1,6 @@
+﻿namespace _Blobs.Interfaces
+{
+    public interface IBehavior
+    {
+    }
+}
